57bcab6960e1f7be7e170b83e257b4a4af2653cf
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-07-16 19:36:38 -0400

    Added test data reset capability to Startup.cs

---
+++ test/CareTogether.TestData/TestStorageHelper.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Storage.Blobs;
+using System;
+
+namespace CareTogether.TestData
+{
+    public static class TestStorageHelper
+    {
+        public static void ResetTestTenantData(BlobServiceClient blobServiceClient)
+        {
+            var organizationId = guid1.ToString();
+            var tenantContainer = blobServiceClient.GetBlobContainerClient(organizationId);
+            tenantContainer.DeleteIfExists();
+            tenantContainer.Create();
+        }
+
+        private static Guid Id(char x) => Guid.Parse("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx".Replace('x', x));
+        static readonly Guid guid1 = Id('1');
+    }
+}
