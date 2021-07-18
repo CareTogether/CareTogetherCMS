e985eb57ae3030bafe947b2f8d8926bb765f4a86
David Malament <davidmalament@davids-mini-2.lan>
2021-07-18 13:12:48 -0400

    Issue 1: draft commit for review

---
+++ src/CareTogether.Core/LogType.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CareTogether
+{
+    public enum LogType
+    {
+        COMMUNITY_EVENT, CONTACT_COMMAND_EXECUTED_EVENT, GOAL_COMMAND_EXECUTED_EVENT, REFERRAL_EVENT
+    }
+}
---
+++ src/CareTogether.Core/Utilities/AppendBlobMultitenantEventLog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Newtonsoft.Json;
+using OneOf;
+using OneOf.Types;
+
+
+namespace CareTogether.Utilities
+{
+    public class AppendBlobMultitenantEventLog<T>
+    {
+        private LogType _logType;
+        private BlobServiceClient _blobServiceClient;
+
+        public AppendBlobMultitenantEventLog(BlobServiceClient blobServiceClient, LogType logType)
+        {
+            this._blobServiceClient = blobServiceClient;
+            this._logType = logType;        
+        }
+
+        public async Task<OneOf<Success, Error>> AppendEventAsync(Guid organizationId, Guid locationId, T domainEvent, long expectedSequenceNumber)
+        {
+            var tenantContainer = _blobServiceClient.GetBlobContainerClient(organizationId.ToString());
+
+            var tenantBlob = tenantContainer.GetAppendBlobClient($"{locationId}/{_logType.ConvertToString()}/00001.ndjson");
+
+            if(!tenantBlob.Exists())
+            {
+                await tenantBlob.CreateAsync();
+                await tenantBlob.SetHttpHeadersAsync(new BlobHttpHeaders() { ContentType = "application/x-ndjson" });
+            }
+
+            var eventJson = JsonConvert.SerializeObject(domainEvent);
+            var cleanEventJson = eventJson.Replace("\n", "").Replace("\r", "");
+            var eventStream = new MemoryStream(Encoding.UTF8.GetBytes(cleanEventJson));
+
+            var appendReturn = (BlobAppendInfo) await tenantBlob.AppendBlockAsync(eventStream);
+            
+            if(appendReturn.BlobCommittedBlockCount == expectedSequenceNumber)
+            {
+                return new Success();
+            }
+            else
+            {
+                return new Error();
+            }
+        }
+
+        public IAsyncEnumerable<(T DomainEvent, long SequenceNumber)> GetAllEventsAsync(Guid organizationId, Guid locationId)
+        {
+            return null;
+        }
+    }
+}
