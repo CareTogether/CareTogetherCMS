6ea1f28bbadd077cfbeaaa951d38ecb35ed1c064
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-07-24 14:35:39 -0400

    Removing unused code from an older Notes design

--- src/CareTogether.Contracts/Resources/IFormsResource.cs
+++ src/CareTogether.Contracts/Resources/IFormsResource.cs
@@ -8,36 +8,5 @@ namespace CareTogether.Resources
 {
     public interface IFormsResource
     {
-        //TODO: Where do we track *which forms belong to a referral*? That appears to require the referral ID being used as a lookup ID in the IFormsResource.
-
-        public Guid CreateDraftNote(Guid arrangementId, NoteContents contents)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ApproveDraftNote(Guid arrangementId, Guid noteId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DenyDraftNote(Guid arrangementId, Guid noteId, DraftNoteDenialReason reason)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void EditDraftNote(Guid arrangementId, Guid noteId, NoteContents contents)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Note> GetNotes(Guid arrangementId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Note GetNote(Guid arrangementId, Guid noteId)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
