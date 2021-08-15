dfacb96ef3ad53e307190742f199a1586c2780ef
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-08-15 18:25:13 -0400

    Initial draft of volunteer family panel

--- src/caretogether-pwa/src/Components/Volunteers.tsx
+++ src/caretogether-pwa/src/Components/Volunteers.tsx
@@ -1,12 +1,14 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Paper, Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Drawer } from '@material-ui/core';
-import { ExactAge, AgeInYears, VolunteerFamily } from '../GeneratedClient';
+import { Grid, Paper, Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Drawer, Container, Toolbar, Button, Menu, MenuItem, Divider } from '@material-ui/core';
+import { ExactAge, AgeInYears, VolunteerFamily, FormUploadRequirement, ActivityRequirement, VolunteerFamilyRequirementScope } from '../GeneratedClient';
 import { differenceInYears } from 'date-fns';
 import { useRecoilValue } from 'recoil';
 import { volunteerFamiliesData } from '../Model/VolunteerFamiliesModel';
 import { policyData } from '../Model/ConfigurationModel';
 import { RoleApprovalStatus } from '../GeneratedClient';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import React, { useState } from 'react';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -29,6 +31,15 @@ const useStyles = makeStyles((theme) => ({
   childRow: {
     color: 'ddd',
     fontStyle: 'italic'
+  },
+  drawerPaper: {
+    //position: 'relative',
+    //whiteSpace: 'nowrap',
+    width: 800,
+    // transition: theme.transitions.create('width', {
+    //   easing: theme.transitions.easing.sharp,
+    //   duration: theme.transitions.duration.enteringScreen,
+    // }),
   }
 }));
 
@@ -50,9 +61,37 @@ function Volunteers() {
     Object.entries(policy.volunteerPolicy?.volunteerRoles).map(([key]) => key))
     || [];
 
-  // const [drawerOpen, setDrawerOpen] = useState(false);
-
   const [selectedVolunteerFamily, setSelectedVolunteerFamily] = useState<VolunteerFamily | null>(null);
+
+  const [familyAddMenuAnchor, setFamilyAddMenuAnchor] = React.useState<Element | null>(null);
+
+  const familyDocumentTypes = (policy.volunteerPolicy?.volunteerFamilyRoles &&
+    Object.entries(policy.volunteerPolicy.volunteerFamilyRoles)
+      .reduce((previous, [, familyRolePolicy]) => {
+        const formUploads = familyRolePolicy.approvalRequirements
+          ?.filter(requirement => requirement.actionRequirement instanceof FormUploadRequirement && requirement.scope === VolunteerFamilyRequirementScope.OncePerFamily)
+          ?.map(requirement => requirement.actionRequirement as FormUploadRequirement) || [];
+        return previous.concat(formUploads);
+      }, [] as FormUploadRequirement[])
+      .reduce((previous, familyFormUploadRequirement) => {
+        return previous.filter(x => x.formName === familyFormUploadRequirement.formName).length > 0
+          ? previous
+          : previous.concat(familyFormUploadRequirement);
+      }, [] as FormUploadRequirement[])) || [];
+      
+  const familyActivityTypes = (policy.volunteerPolicy?.volunteerFamilyRoles &&
+  Object.entries(policy.volunteerPolicy.volunteerFamilyRoles)
+    .reduce((previous, [, familyRolePolicy]) => {
+      const activities = familyRolePolicy.approvalRequirements
+        ?.filter(requirement => requirement.actionRequirement instanceof ActivityRequirement && requirement.scope === VolunteerFamilyRequirementScope.OncePerFamily)
+        ?.map(requirement => requirement.actionRequirement as ActivityRequirement) || [];
+      return previous.concat(activities);
+    }, [] as ActivityRequirement[])
+    .reduce((previous, familyActivityRequirement) => {
+      return previous.filter(x => x.activityName === familyActivityRequirement.activityName).length > 0
+        ? previous
+        : previous.concat(familyActivityRequirement);
+    }, [] as ActivityRequirement[])) || [];
 
   return (
     <Grid container spacing={3}>
@@ -128,8 +167,54 @@ function Volunteers() {
             </TableBody>
           </Table>
         </TableContainer>
-        <Drawer anchor={'right'} open={selectedVolunteerFamily !== null} onClose={() => setSelectedVolunteerFamily(null)}>
-          Volunteer Family &amp; Individual Records
+        <Drawer anchor={'right'} classes={{
+            paper: clsx(classes.drawerPaper),
+          }} open={selectedVolunteerFamily !== null} onClose={() => setSelectedVolunteerFamily(null)}>
+          <Container>
+            <Toolbar variant="dense" disableGutters={true}>
+              <h3>Family</h3>
+              &nbsp;
+              <Button aria-controls="family-add-menu" aria-haspopup="true"
+                onClick={(event) => setFamilyAddMenuAnchor(event.currentTarget)}>
+                <AddCircleIcon />
+              </Button>
+              <Menu id="family-add-menu"
+                anchorEl={familyAddMenuAnchor}
+                open={Boolean(familyAddMenuAnchor)}
+                onClose={() => setFamilyAddMenuAnchor(null)}>
+                {familyDocumentTypes.map(familyDocumentType => (
+                  <MenuItem key={familyDocumentType.formName}>{familyDocumentType.formName}</MenuItem>
+                ))}
+                <Divider />
+                {familyActivityTypes.map(familyActivityType => (
+                  <MenuItem key={familyActivityType.activityName}>{familyActivityType.activityName}</MenuItem>
+                ))}
+              </Menu>
+            </Toolbar>
+            <ul>
+              {selectedVolunteerFamily?.approvalFormUploads?.map((upload, i) => (
+                <li key={i}>{upload.formName} @ {upload.timestampUtc?.toDateString()}</li>
+              ))}
+            </ul>
+            <Toolbar variant="dense" disableGutters={true}>
+              <h3>Adults</h3>
+              &nbsp;
+              🏗
+            </Toolbar>
+            {selectedVolunteerFamily?.family?.adults?.map(adult => (
+              <h4 key={adult.item1?.id}>{adult.item1?.firstName} {adult.item1?.lastName}</h4>
+            ))}
+            <Toolbar variant="dense" disableGutters={true}>
+              <h3>Children</h3>
+              &nbsp;
+              🏗
+            </Toolbar>
+            {selectedVolunteerFamily?.family?.children?.map(child => (
+              <h4 key={child.id}>{child.firstName} {child.lastName}</h4>
+            ))}
+            <hr />
+            <p>{JSON.stringify(selectedVolunteerFamily)}</p>
+          </Container>
         </Drawer>
       </Grid>
     </Grid>
