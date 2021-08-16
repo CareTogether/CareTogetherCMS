0c86d5c75020c71934e7e50d0cf01507583cad99
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-08-16 18:14:06 -0400

    Extracting more reusable code for model management

--- src/caretogether-pwa/src/Model/VolunteerFamiliesModel.ts
+++ src/caretogether-pwa/src/Model/VolunteerFamiliesModel.ts
@@ -1,4 +1,4 @@
-import { atom, RecoilState, Snapshot, useRecoilCallback } from "recoil";
+import { atom, useRecoilCallback } from "recoil";
 import { FormUploadRequirement, UploadVolunteerFamilyForm, VolunteerFamiliesClient, VolunteerFamily, VolunteerFamilyCommand } from "../GeneratedClient";
 import { authenticatingFetch } from "../Auth";
 import { currentOrganizationState, currentLocationState } from "./SessionModel";
@@ -9,39 +9,42 @@ export const volunteerFamiliesData = atom<VolunteerFamily[]>({
   default: []
 });
 
-async function runVolunteerFamilyCommandCallback(snapshot: Snapshot, set: <T>(recoilVal: RecoilState<T>, valOrUpdater: T | ((currVal: T) => T)) => void,
-  volunteerFamilyId: string, callback: (organizationId: string, locationId: string) => Promise<VolunteerFamilyCommand>) {
-  const organizationId = await snapshot.getPromise(currentOrganizationState);
-  const locationId = await snapshot.getPromise(currentLocationState);
+function useVolunteerFamilyCommandCallback<T>(
+  callback: (organizationId: string, locationId: string, volunteerFamilyId: string, ...args: T[]) => Promise<VolunteerFamilyCommand>) {
+  return useRecoilCallback(({snapshot, set}) => {
+    const asyncCallback = async (volunteerFamilyId: string, ...args: T[]) => {
+      const organizationId = await snapshot.getPromise(currentOrganizationState);
+      const locationId = await snapshot.getPromise(currentLocationState);
 
-  const command = await callback(organizationId, locationId);
-  const vfc = new VolunteerFamiliesClient(process.env.REACT_APP_API_HOST, authenticatingFetch);
+      const command = await callback(organizationId, locationId, volunteerFamilyId, ...args);
 
-  const updatedFamily = await vfc.submitVolunteerFamilyCommand(organizationId, locationId, command);
+      const client = new VolunteerFamiliesClient(process.env.REACT_APP_API_HOST, authenticatingFetch);
+      const updatedFamily = await client.submitVolunteerFamilyCommand(organizationId, locationId, command);
 
-  set(volunteerFamiliesData, current => {
-    return current.map(currentEntry => currentEntry.family?.id === volunteerFamilyId
-      ? updatedFamily
-      : currentEntry);
-  });
+      set(volunteerFamiliesData, current => {
+        return current.map(currentEntry => currentEntry.family?.id === volunteerFamilyId
+          ? updatedFamily
+          : currentEntry);
+      });
+    };
+    return asyncCallback;
+  })
 }
 
 export function useVolunteerFamiliesModel() {
-  const uploadForm = useRecoilCallback(({snapshot, set}) =>
-    async (volunteerFamilyId: string, requirement: FormUploadRequirement, formFile: File) => {
-      await runVolunteerFamilyCommandCallback(snapshot, set, volunteerFamilyId, async (organizationId, locationId) => {
-        const uploadedDocumentId = await uploadFileToTenant(organizationId, locationId, formFile);
+  const uploadForm = useVolunteerFamilyCommandCallback(
+    async (organizationId, locationId, volunteerFamilyId, [requirement, formFile]:[FormUploadRequirement, File]) => {
+      const uploadedDocumentId = await uploadFileToTenant(organizationId, locationId, formFile);
 
-        const uploadCommand = new UploadVolunteerFamilyForm({
-          familyId: volunteerFamilyId
-        });
-        uploadCommand.formName = requirement.formName;
-        uploadCommand.formVersion = requirement.formVersion;
-        uploadCommand.uploadedDocumentId = uploadedDocumentId;
-        uploadCommand.uploadedFileName = formFile.name;
-
-        return uploadCommand;
+      const uploadCommand = new UploadVolunteerFamilyForm({
+        familyId: volunteerFamilyId
       });
+      uploadCommand.formName = requirement.formName;
+      uploadCommand.formVersion = requirement.formVersion;
+      uploadCommand.uploadedDocumentId = uploadedDocumentId;
+      uploadCommand.uploadedFileName = formFile.name;
+
+      return uploadCommand;
     });
   
   return {
