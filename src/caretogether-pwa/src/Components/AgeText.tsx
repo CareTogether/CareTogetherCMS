774fd7790a5a334bcdb27567e48657760c93679f
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-09-19 22:00:31 -0400

    Removing calendar emoji from date-of-birth ages

--- src/caretogether-pwa/src/Components/AgeText.tsx
+++ src/caretogether-pwa/src/Components/AgeText.tsx
@@ -9,7 +9,7 @@ export function AgeText({age}: AgeTextProps) {
   return (
     <span className="age">{
       age && age instanceof ExactAge
-        ? age.dateOfBirth && "📅" + differenceInYears(new Date(), age.dateOfBirth)
+        ? age.dateOfBirth && differenceInYears(new Date(), age.dateOfBirth)
         : age instanceof AgeInYears
         ? age.years && age.asOf && (age.years + differenceInYears(new Date(), age.asOf))
         : "⚠"
