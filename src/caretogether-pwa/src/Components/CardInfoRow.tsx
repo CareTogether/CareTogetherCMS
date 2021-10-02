c7ef176cadb41eec38631635fb7a920e92d3c010
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-10-02 18:15:05 -0400

    Fixed nested <p> tags

--- src/caretogether-pwa/src/Components/CardInfoRow.tsx
+++ src/caretogether-pwa/src/Components/CardInfoRow.tsx
@@ -7,9 +7,9 @@ export const CardInfoRow: React.FC<CardInfoRowProps> = ({
   children
 }) => (
   <>
-    <p style={{lineHeight: 1, marginTop: 10, marginBottom: 10}}>
+    <div style={{lineHeight: 1, marginTop: 10, marginBottom: 10}}>
       <span style={{display: 'inline-block', width: 30}}>{icon}</span>
       {children}
-    </p>
+    </div>
   </>
 );
