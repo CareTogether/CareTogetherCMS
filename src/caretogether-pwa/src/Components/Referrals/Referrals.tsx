6103d29afdbb78872ac01abd0af805ac61835581
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-11-06 16:48:07 -0400

    Listing partnering families!!

--- src/caretogether-pwa/src/Components/Referrals/Referrals.tsx
+++ src/caretogether-pwa/src/Components/Referrals/Referrals.tsx
@@ -1,8 +1,10 @@
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import { useRecoilValue } from 'recoil';
 import { partneringFamiliesData } from '../../Model/ReferralsModel';
+import { differenceInYears } from 'date-fns';
+import React from 'react';
+import { CombinedFamilyInfo, Gender, ExactAge, AgeInYears } from '../../GeneratedClient';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -14,7 +16,30 @@ const useStyles = makeStyles((theme) => ({
   fixedHeight: {
     height: 240,
   },
+  table: {
+    minWidth: 700,
+  },
+  familyRow: {
+    backgroundColor: '#eef'
+  },
+  adultRow: {
+  },
+  childRow: {
+    color: 'ddd',
+    fontStyle: 'italic'
+  },
+  drawerPaper: {
+  },
+  fabAdd: {
+    position: 'fixed',
+    right: '30px',
+    bottom: '70px'
+  }
 }));
+
+function familyLastName(family: CombinedFamilyInfo) {
+  return family.family!.adults?.filter(adult => family.family!.primaryFamilyContactPersonId === adult.item1?.id)[0]?.item1?.lastName || "";
+}
 
 function Referrals() {
   const classes = useStyles();
@@ -23,25 +48,72 @@ function Referrals() {
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        <Paper className={clsx(classes.paper)}>
-          {/* <button onClick={refreshReferrals}>🔃 Refresh Referrals</button> */}
-          <br />
-          <table>
-            <thead>
-              <tr>
-                {/* TODO: Render the family name as the name of the primary family contact */}
-                <th>Data as JSON for testing</th>
-              </tr>
-            </thead>
-            <tbody>
-              {partneringFamilies.map(partneringFamily => (
-                <tr key={partneringFamily.family!.id}>
-                  <td>{JSON.stringify(partneringFamily.partneringFamilyInfo)}<br />================</td>
-                </tr>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>First Name</TableCell>
+                <TableCell>Last Name</TableCell>
+                <TableCell>Gender</TableCell>
+                <TableCell>Age</TableCell>
+                {/* TODO: Additional columns */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {partneringFamilies.map((partneringFamily) => (
+                <React.Fragment key={partneringFamily.family?.id}>
+                  <TableRow className={classes.familyRow} /*onClick={() => openPartneringFamily(partneringFamily.family!.id!)}*/>
+                    <TableCell key="1" colSpan={4}>{familyLastName(partneringFamily) + " Family"
+                    }</TableCell>
+                    {/* TODO: Additional columns */}
+                  </TableRow>
+                  {partneringFamily.family?.adults?.map(adult => adult.item1 && (
+                    <TableRow key={partneringFamily.family?.id + ":" + adult.item1.id}
+                      //onClick={() => openPartneringFamily(volunteerFamily.family!.id!)}
+                      className={classes.adultRow}>
+                      <TableCell>{adult.item1.firstName}</TableCell>
+                      <TableCell>{adult.item1.lastName}</TableCell>
+                      <TableCell>{typeof(adult.item1.gender) === 'undefined' ? "" : Gender[adult.item1.gender]}</TableCell>
+                      <TableCell align="right">
+                        { adult.item1.age instanceof ExactAge
+                          ? adult.item1.age.dateOfBirth && differenceInYears(new Date(), adult.item1.age.dateOfBirth)
+                          : adult.item1.age instanceof AgeInYears
+                          ? adult.item1.age.years && adult.item1?.age.asOf && (adult.item1.age.years + differenceInYears(new Date(), adult.item1.age.asOf))
+                          : "⚠" }
+                      </TableCell>
+                      {/* TODO: Additional columns */}
+                    </TableRow>
+                  ))}
+                  {partneringFamily.family?.children?.map(child => (
+                    <TableRow key={partneringFamily.family?.id + ":" + child.id}
+                      //onClick={() => openPartneringFamily(volunteerFamily.family!.id!)}
+                      className={classes.childRow}>
+                      <TableCell>{child.firstName}</TableCell>
+                      <TableCell>{child.lastName}</TableCell>
+                      <TableCell>{typeof(child.gender) === 'undefined' ? "" : Gender[child.gender]}</TableCell>
+                      <TableCell align="right">
+                        { child.age instanceof ExactAge
+                          ? child.age.dateOfBirth && differenceInYears(new Date(), child.age.dateOfBirth)
+                          : child.age instanceof AgeInYears
+                          ? child.age.years && child.age.asOf && (child.age.years + differenceInYears(new Date(), child.age.asOf))
+                          : "⚠" }
+                      </TableCell>
+                      {/* TODO: Additional columns */}
+                    </TableRow>
+                  ))}
+                </React.Fragment>
               ))}
-            </tbody>
-          </table>
-        </Paper>
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {/* <Fab color="primary" aria-label="add" className={classes.fabAdd}
+          onClick={() => setCreatePartneringFamilyDialogOpen(true)}>
+          <AddIcon />
+        </Fab> */}
+        {/* {createPartneringFamilyDialogOpen && <CreatePartneringFamilyDialog onClose={(partneringFamilyId) => {
+          setCreatePartneringFamilyDialogOpen(false);
+          partneringFamilyId && history.push(`/referrals/family/${partneringFamilyId}`);
+        }} />} */}
       </Grid>
     </Grid>
   );
