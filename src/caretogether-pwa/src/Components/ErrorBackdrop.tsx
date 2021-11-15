1a6481de7d4527ba16b9fb942fb34250ae4c0249
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-11-14 20:05:54 -0500

    Line wrapping for error overlay

--- src/caretogether-pwa/src/Components/ErrorBackdrop.tsx
+++ src/caretogether-pwa/src/Components/ErrorBackdrop.tsx
@@ -24,8 +24,8 @@ export default function ErrorBackdrop() {
       <div style={{backgroundColor: '#fff', padding: 20, borderStyle: 'solid', borderRadius: 20, borderColor: '#500', borderWidth: 3, maxWidth: 500 }}>
         <h2>🤓 Ooops! Time to email tech support. 📧</h2>
         <p>Looks like something went wrong! When you reach out for help, please include a screenshot and a description (as precise as possible) of the steps you took to get here.</p>
-        <pre>Error: {JSON.stringify(errorInfo)}</pre>
-        <pre>URL: {window.location.toString()}</pre>
+        <pre style={{whiteSpace: 'pre-wrap'}}>Error: {JSON.stringify(errorInfo)}</pre>
+        <pre style={{whiteSpace: 'pre-wrap'}}>URL: {window.location.toString()}</pre>
       </div>
     </Backdrop>
   );
