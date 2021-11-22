9c84eb28b4553a6c37c3d64f628b7b0a2a638b71
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-11-22 15:29:52 -0500

    Draft of updating the CombinedFamilyInfo contract for documents

--- src/CareTogether.Contracts/Managers/SharedContracts.cs
+++ src/CareTogether.Contracts/Managers/SharedContracts.cs
@@ -7,7 +7,7 @@ namespace CareTogether.Managers
 {
     public sealed record CombinedFamilyInfo(Family Family,
         PartneringFamilyInfo? PartneringFamilyInfo, VolunteerFamilyInfo? VolunteerFamilyInfo,
-        ImmutableList<Note> Notes);
+        ImmutableList<Note> Notes, ImmutableList<UploadedDocumentInfo> UploadedDocuments);
 
     public sealed record PartneringFamilyInfo(
         Referral? OpenReferral,
@@ -16,14 +16,12 @@ namespace CareTogether.Managers
     public sealed record Referral(Guid Id,
         DateTime OpenedAtUtc, DateTime? ClosedAtUtc, ReferralCloseReason? CloseReason,
         ImmutableList<CompletedRequirementInfo> CompletedRequirements,
-        ImmutableList<UploadedDocumentInfo> UploadedDocuments,
         ImmutableList<string> MissingIntakeRequirements,
         ImmutableList<Arrangement> Arrangements);
 
     public sealed record Arrangement(Guid Id, string ArrangementType,
         ArrangementPhase Phase, DateTime RequestedAtUtc, DateTime? StartedAtUtc, DateTime? EndedAtUtc,
         ImmutableList<CompletedRequirementInfo> CompletedRequirements,
-        ImmutableList<UploadedDocumentInfo> UploadedDocuments,
         ImmutableList<string> MissingSetupRequirements,
         ImmutableList<string> MissingMonitoringRequirements, //TODO: Better define this, especially when recurrence is involved
         ImmutableList<string> MissingCloseoutRequirements,
@@ -37,7 +35,6 @@ namespace CareTogether.Managers
 
     public sealed record VolunteerFamilyInfo(
         ImmutableList<CompletedRequirementInfo> CompletedRequirements,
-        ImmutableList<UploadedDocumentInfo> UploadedDocuments,
         ImmutableList<RemovedRole> RemovedRoles,
         ImmutableList<string> MissingRequirements,
         ImmutableList<string> AvailableApplications,
