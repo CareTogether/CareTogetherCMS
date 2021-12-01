9ef46f2a9fbf7d1aef40c472f9fc901ac00dea74
Lars Kemmann <lars.kemmann@bynalogic.com>
2021-11-30 21:11:26 -0500

    Opening a new referral after one has been closed

---
+++ src/caretogether-pwa/src/Components/Referrals/OpenNewReferralDialog.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { Grid } from '@material-ui/core';
+import { UpdateDialog } from '../UpdateDialog';
+import { KeyboardDatePicker } from '@material-ui/pickers';
+import { useReferralsModel } from '../../Model/ReferralsModel';
+
+interface OpenNewReferralDialogProps {
+  partneringFamilyId: string,
+  onClose: () => void
+}
+
+export function OpenNewReferralDialog({partneringFamilyId, onClose}: OpenNewReferralDialogProps) {
+  const referralsModel = useReferralsModel();
+  const [fields, setFields] = useState({
+    openedAtLocal: new Date()
+  });
+  const { openedAtLocal } = fields;
+
+  async function save() {
+    await referralsModel.openReferral(partneringFamilyId,
+      openedAtLocal);
+  }
+
+  return (
+    <UpdateDialog title={`Open a new referral`} onClose={onClose}
+      onSave={save}>
+      <form noValidate autoComplete="off">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <KeyboardDatePicker
+              label="When was this referral opened?"
+              value={openedAtLocal} fullWidth required
+              disableFuture format="MM/dd/yyyy"
+              onChange={(date) => date && setFields({...fields, openedAtLocal: date})}
+              showTodayButton />
+          </Grid>
+        </Grid>
+      </form>
+    </UpdateDialog>
+  );
+}
