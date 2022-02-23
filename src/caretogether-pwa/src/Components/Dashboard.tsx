527ab83c873b879aead2b564c2444f93501518aa
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-02-22 20:06:18 -0500

    Fixed dashboard welcome screen

--- src/caretogether-pwa/src/Components/Dashboard.tsx
+++ src/caretogether-pwa/src/Components/Dashboard.tsx
@@ -3,29 +3,28 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 240,
-    },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
 }));
 
 function Dashboard() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12} md={8} lg={9}>
-                <Paper className={clsx(classes.paper, classes.fixedHeight)}>
-                    Area 1
-                </Paper>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container spacing={3}>
+      <Grid item style={{textAlign: 'center', margin: 12}}>
+        <h1>Welcome to the CareTogether Case Management System!</h1>
+        <p>Select an option from the menu to begin.</p>
+      </Grid>
+    </Grid>
+  );
 }
 
 export { Dashboard };
