81589a779003b18b4d964b500fb4fcaf5a49eadd
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-03-12 17:55:01 -0500

    Removed unnecessary wrapping fragment from IconRow

--- src/caretogether-pwa/src/Components/IconRow.tsx
+++ src/caretogether-pwa/src/Components/IconRow.tsx
@@ -6,10 +6,8 @@ export const IconRow: React.FC<IconRowProps> = ({
   icon,
   children
 }) => (
-  <>
-    <div style={{lineHeight: 1, marginTop: 10, marginBottom: 10}}>
-      <span style={{display: 'inline-block', width: 30}}>{icon}</span>
-      {children}
-    </div>
-  </>
+  <div style={{lineHeight: 1, marginTop: 10, marginBottom: 10}}>
+    <span style={{display: 'inline-block', width: 30}}>{icon}</span>
+    {children}
+  </div>
 );
