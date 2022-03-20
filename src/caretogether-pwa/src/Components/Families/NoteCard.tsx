f4f85738bb88c90ac4273926ed3f76841a99a584
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-03-20 11:01:30 -0400

    Cleaned up Note display

--- src/caretogether-pwa/src/Components/Families/NoteCard.tsx
+++ src/caretogether-pwa/src/Components/Families/NoteCard.tsx
@@ -15,7 +15,7 @@ import React, { useState } from 'react';
 import { Note, NoteStatus } from '../../GeneratedClient';
 import { useUserLookup } from '../../Model/DirectoryModel';
 import { PersonName } from './PersonName';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import CheckIcon from '@mui/icons-material/Check';
 import EditIcon from '@mui/icons-material/Edit';
 import { AddEditNoteDialog } from './AddEditNoteDialog';
@@ -64,13 +64,7 @@ export function NoteCard({ familyId, note }: NoteCardProps) {
 
   const userLookup = useUserLookup();
 
-  const [moreMenuAnchor, setMoreMenuAnchor] = useState<{anchor: Element} | null>(null);
   const [showDiscardNoteDialog, setShowDiscardNoteDialog] = useState(false);
-  function selectDiscardDraftNote() {
-    setMoreMenuAnchor(null);
-    setShowDiscardNoteDialog(true);
-  }
-  
   const [showApproveNoteDialog, setShowApproveNoteDialog] = useState(false);
   const [showEditNoteDialog, setShowEditNoteDialog] = useState(false);
 
@@ -80,22 +74,23 @@ export function NoteCard({ familyId, note }: NoteCardProps) {
         subheader={<>
           <PersonName person={userLookup(note.authorId)} />
           {note.status === NoteStatus.Draft ? " - DRAFT" : ""}
-        </>}
-        action={
-          note.status === NoteStatus.Draft && (
-          <IconButton
-            onClick={(event) => setMoreMenuAnchor({anchor: event.currentTarget})}
-            size="large">
-            <MoreVertIcon />
-          </IconButton>
-        )} />
+        </>} />
       <CardContent className={classes.cardContent}>
         <Typography variant="body2" component="p">
           {note.contents}
         </Typography>
       </CardContent>
-      <CardActions className={classes.cardActions}>
-        {note.status === NoteStatus.Draft && (
+      {note.status === NoteStatus.Draft &&
+        <CardActions className={classes.cardActions}>
+          <Button
+            onClick={() => setShowDiscardNoteDialog(true)}
+            variant="contained"
+            size="small"
+            color='secondary'
+            className={classes.rightCardActionButton}
+            startIcon={<DeleteForeverIcon />}>
+            Discard
+          </Button>
           <Button
             onClick={() => setShowEditNoteDialog(true)}
             variant="contained"
@@ -104,8 +99,6 @@ export function NoteCard({ familyId, note }: NoteCardProps) {
             startIcon={<EditIcon />}>
             Edit
           </Button>
-        )}
-        {note.status === NoteStatus.Draft && (
           <Button
             onClick={() => setShowApproveNoteDialog(true)}
             variant="contained"
@@ -114,19 +107,7 @@ export function NoteCard({ familyId, note }: NoteCardProps) {
             startIcon={<CheckIcon />}>
             Approve
           </Button>
-        )}
-      </CardActions>
-      <Menu id="note-more-menu"
-        anchorEl={moreMenuAnchor?.anchor}
-        keepMounted
-        open={Boolean(moreMenuAnchor)}
-        onClose={() => setMoreMenuAnchor(null)}>
-        {note.status === NoteStatus.Draft && (
-          <MenuItem onClick={selectDiscardDraftNote}>
-            <ListItemText primary="Discard draft note" />
-          </MenuItem>
-        )}
-      </Menu>
+        </CardActions>}
       {(showDiscardNoteDialog && <DiscardNoteDialog familyId={familyId} note={note}
         onClose={() => setShowDiscardNoteDialog(false)} />) || null}
       {(showApproveNoteDialog && <ApproveNoteDialog familyId={familyId} note={note}
