cecc925e5f03f8f03e503d13ca53e414c1f24103
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-03-25 21:15:43 -0400

    Extracted ArrangementPhaseSummary component

--- src/caretogether-pwa/src/Components/Referrals/ArrangementCard.tsx
+++ src/caretogether-pwa/src/Components/Referrals/ArrangementCard.tsx
@@ -10,18 +10,17 @@ import {
   Menu,
   MenuItem,
   MenuList,
-  Tooltip,
   Typography,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ArrangementPhase, Arrangement, CombinedFamilyInfo, FunctionRequirement, ArrangementFunction, ChildInvolvement } from '../../GeneratedClient';
 import { useFamilyLookup, usePersonLookup } from '../../Model/DirectoryModel';
 import { PersonName } from '../Families/PersonName';
 import { FamilyName } from '../Families/FamilyName';
-import { format, formatRelative } from 'date-fns';
+import { formatRelative } from 'date-fns';
 import { IconRow } from '../IconRow';
 import { useRecoilValue } from 'recoil';
 import { policyData } from '../../Model/ConfigurationModel';
@@ -36,54 +35,7 @@ import { ExemptedRequirementRow } from "../Requirements/ExemptedRequirementRow";
 import { CompletedRequirementRow } from "../Requirements/CompletedRequirementRow";
 import { ArrangementContext } from "../Requirements/RequirementContext";
 import { CancelArrangementDialog } from './CancelArrangementDialog';
-
-type ArrangementPhaseSummaryProps = {
-  phase: ArrangementPhase,
-  requestedAtUtc: Date,
-  startedAtUtc?: Date,
-  endedAtUtc?: Date
-}
-
-function ArrangementPhaseSummary({ phase, requestedAtUtc, startedAtUtc, endedAtUtc }: ArrangementPhaseSummaryProps) {
-  const cancelledColor = "#bdbdbd";
-  const completedPhaseColor = "#00838f";
-  const currentPhaseColor = "#ffc400";
-  const futurePhaseColor = "#ddd";
-  return (
-    <Tooltip title={<>
-      <p>Requested at {format(requestedAtUtc, "M/d/yy h:mm a")}</p>
-      {startedAtUtc && <p>Started at {format(startedAtUtc, "M/d/yy h:mm a")}</p>}
-      {endedAtUtc && <p>Ended at {format(endedAtUtc, "M/d/yy h:mm a")}</p>}
-    </>}>
-      <div style={{display: "flex", height: 8, backgroundColor: "red"}}>
-        <div style={{flexGrow: 1,
-          backgroundColor:
-            phase === ArrangementPhase.Cancelled ? cancelledColor
-            : phase === ArrangementPhase.SettingUp ? currentPhaseColor
-            : phase === ArrangementPhase.ReadyToStart ? completedPhaseColor
-            : phase === ArrangementPhase.Started ? completedPhaseColor
-            : completedPhaseColor}}>
-        </div>
-        <div style={{flexGrow: 1,
-          backgroundColor:
-            phase === ArrangementPhase.Cancelled ? cancelledColor
-            : phase === ArrangementPhase.SettingUp ? futurePhaseColor
-            : phase === ArrangementPhase.ReadyToStart ? futurePhaseColor
-            : phase === ArrangementPhase.Started ? currentPhaseColor
-            : completedPhaseColor}}>
-        </div>
-        <div style={{flexGrow: 1,
-          backgroundColor:
-            phase === ArrangementPhase.Cancelled ? cancelledColor
-            : phase === ArrangementPhase.SettingUp ? futurePhaseColor
-            : phase === ArrangementPhase.ReadyToStart ? futurePhaseColor
-            : phase === ArrangementPhase.Started ? futurePhaseColor
-            : completedPhaseColor /* TODO: Show as currentPhaseColor if any closeout requirements are missing */}}>
-        </div>
-      </div>
-    </Tooltip>
-  );
-}
+import { ArrangementPhaseSummary } from './ArrangementPhaseSummary';
 
 const useStyles = makeStyles((theme) => ({
   card: {
