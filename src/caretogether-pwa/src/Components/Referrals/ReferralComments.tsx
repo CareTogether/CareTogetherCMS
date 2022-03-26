c688611fa29c5f16993d3876f82521250f787b9c
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-03-25 20:13:42 -0400

    Extracted common 'useInlineEditor' hook for inline editing behavior

--- src/caretogether-pwa/src/Components/Referrals/ReferralComments.tsx
+++ src/caretogether-pwa/src/Components/Referrals/ReferralComments.tsx
@@ -1,11 +1,7 @@
-import { Button, TextField } from "@mui/material";
-import EditIcon from '@mui/icons-material/Edit';
-import SaveIcon from '@mui/icons-material/Save';
-import UndoIcon from '@mui/icons-material/Undo';
-import { useState } from "react";
+import { TextField } from "@mui/material";
 import { CombinedFamilyInfo } from "../../GeneratedClient";
 import { useReferralsModel } from "../../Model/ReferralsModel";
-import { useBackdrop } from "../RequestBackdrop";
+import { useInlineEditor } from "../../useInlineEditor";
 
 type ReferralCommentsProps = {
   partneringFamily: CombinedFamilyInfo
@@ -16,63 +12,27 @@ export function ReferralComments({ partneringFamily, referralId }: ReferralComme
   const savedValue = partneringFamily.partneringFamilyInfo?.openReferral?.comments;
 
   const referralsModel = useReferralsModel();
-  const withBackdrop = useBackdrop();
 
-  const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState(savedValue);
-  async function saveChanges() {
-    await withBackdrop(async () => {
-      await referralsModel.updateReferralComments(partneringFamily.family!.id!, referralId, value);
-      setEditing(false);
-    });
-  }
-  function cancelEditing() {
-    setEditing(false);
-    setValue(savedValue);
-  }
+  const editor = useInlineEditor(async value => {
+    await referralsModel.updateReferralComments(partneringFamily.family!.id!, referralId, value);
+  }, savedValue);
 
   return (
     <>
       <h3 style={{ marginBottom: 0 }}>
         Comments
-        {!editing && <Button
-          onClick={() => setEditing(true)}
-          variant="text"
-          size="small"
-          startIcon={<EditIcon />}
-          sx={{margin: 1}}>
-          Edit
-        </Button>}
-        {editing &&
-        <>
-          <Button
-            onClick={() => cancelEditing()}
-            variant="contained"
-            size="small"
-            startIcon={<UndoIcon />}
-            color="secondary"
-            sx={{margin: 1}}>
-            Cancel
-          </Button>
-          <Button
-            disabled={value === savedValue}
-            onClick={saveChanges}
-            variant="contained"
-            size="small"
-            startIcon={<SaveIcon />}
-            sx={{margin: 1}}>
-            Save
-          </Button>
-        </>}
+        {editor.editButton}
+        {editor.cancelButton}
+        {editor.saveButton}
       </h3>
-      {editing
+      {editor.editing
         ? <TextField
             id="referral-comments"
             helperText="Referral comments are visible to everyone."
             placeholder="Space for any general notes about the referral, upcoming plans, etc."
             multiline fullWidth variant="outlined" minRows={2} size="medium"
-            value={value}
-            onChange={e => setValue(e.target.value)} />
+            value={editor.value}
+            onChange={e => editor.setValue(e.target.value)} />
         : savedValue}
     </>
   );
