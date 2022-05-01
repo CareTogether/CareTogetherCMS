77f1a43a7451817901c6c3358bea3fcc5e907d66
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-05-01 17:52:43 -0400

    Bugfixes

--- src/caretogether-pwa/src/useCollapsed.ts
+++ src/caretogether-pwa/src/useCollapsed.ts
@@ -12,7 +12,7 @@ class CollapsedDb extends Dexie {
   constructor() {
     super('collapsedItems');
     this.version(1).stores({
-      items: '++id, collapsed' // Primary key and indexed props
+      items: '++id' // Primary key and indexed props (no indexed props in this case)
     });
   }
 }
@@ -22,9 +22,9 @@ const db = new CollapsedDb();
 // Credit: https://usehooks.com/useLocalStorage/
 export function useCollapsed(key: string, initialValue: boolean) {
 
-  async function upsertSavedValue() {
+  async function upsertSavedValue(newValue: boolean) {
     try {
-      await db.items.put({ id: key, collapsed: initialValue });
+      await db.items.put({ id: key, collapsed: newValue }, key);
     } catch (error) {
       console.warn("prefixDb encountered an error");
       console.warn(error);
