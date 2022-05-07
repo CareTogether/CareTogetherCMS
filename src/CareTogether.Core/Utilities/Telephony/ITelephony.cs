49fab42d0eb8b264ca78c83cd7220354a351c842
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-05-07 14:15:15 -0400

    Defined initial interface for telephony utility service

---
+++ src/CareTogether.Core/Utilities/Telephony/ITelephony.cs
@@ -0,0 +1,20 @@
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace CareTogether.Utilities.Telephony
+{
+    public sealed record SmsMessageResult(string PhoneNumber, SmsResult Result);
+    public enum SmsResult
+    {
+        InvalidSourcePhoneNumber,
+        InvalidDestinationPhoneNumber,
+        SendFailure,
+        SendSuccess
+    }
+
+    public interface ITelephony
+    {
+        Task<ImmutableList<SmsMessageResult>> SendSmsMessageAsync(
+            string sourcePhoneNumber, ImmutableList<string> destinationPhoneNumbers, string message);
+    }
+}
