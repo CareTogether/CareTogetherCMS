5b2d66f28027b3e24db537f65303713f0ea64474
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-06-05 14:39:25 -0400

    Showing assignment family/person for missing arrangement requirements

--- src/caretogether-pwa/src/Components/Requirements/MissingArrangementRequirementRow.tsx
+++ src/caretogether-pwa/src/Components/Requirements/MissingArrangementRequirementRow.tsx
@@ -2,8 +2,11 @@ import { format } from "date-fns";
 import { useRecoilValue } from "recoil";
 import { MissingArrangementRequirement, Permission } from "../../GeneratedClient";
 import { policyData } from "../../Model/ConfigurationModel";
+import { useFamilyLookup, usePersonLookup } from "../../Model/DirectoryModel";
 import { usePermissions } from "../../Model/SessionModel";
 import { useDialogHandle } from "../../useDialogHandle";
+import { FamilyName } from "../Families/FamilyName";
+import { PersonName } from "../Families/PersonName";
 import { IconRow } from "../IconRow";
 import { MissingRequirementDialog } from "./MissingRequirementDialog";
 import { RequirementContext } from "./RequirementContext";
@@ -25,16 +28,35 @@ export function MissingArrangementRequirementRow({ requirement, context }: Missi
     ? true //TODO: Implement these permissions!
     : permissions(Permission.EditApprovalRequirementCompletion);
 
+  const familyLookup = useFamilyLookup();
+  const personLookup = usePersonLookup();
+
   return (
     <>
       {requirement.dueBy
         ? <IconRow icon='📅' onClick={canComplete ? dialogHandle.openDialog : undefined}>
           {requirement.actionName}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <span style={{ float: 'right' }}>{format(requirement.dueBy, "M/d/yy h:mm a")}</span>
+          {requirement.volunteerFamilyId && !requirement.personId &&
+            <><br/><span style={{ paddingLeft: '30px', fontStyle: 'italic' }}>
+              <FamilyName family={familyLookup(requirement.volunteerFamilyId)} />
+            </span></>}
+          {requirement.volunteerFamilyId && requirement.personId &&
+            <><br/><span style={{ paddingLeft: '30px', fontStyle: 'italic' }}>
+              <PersonName person={personLookup(requirement.volunteerFamilyId, requirement.personId)} />
+            </span></>}
         </IconRow>
         : <IconRow icon='❌' onClick={canComplete ? dialogHandle.openDialog : undefined}>
           {requirement.actionName}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           {requirement.pastDueSince && <span style={{ float: 'right' }}>{format(requirement.pastDueSince, "M/d/yy h:mm a")}</span>}
+          {requirement.volunteerFamilyId && !requirement.personId &&
+            <><br/><span style={{ paddingLeft: '30px', fontStyle: 'italic' }}>
+              <FamilyName family={familyLookup(requirement.volunteerFamilyId)} />
+            </span></>}
+          {requirement.volunteerFamilyId && requirement.personId &&
+            <><br/><span style={{ paddingLeft: '30px', fontStyle: 'italic' }}>
+              <PersonName person={personLookup(requirement.volunteerFamilyId, requirement.personId)} />
+            </span></>}
         </IconRow>}
       {dialogHandle.open && <MissingRequirementDialog handle={dialogHandle}
         requirement={requirement} context={context} policy={requirementPolicy} />}
