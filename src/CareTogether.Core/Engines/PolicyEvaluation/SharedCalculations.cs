1f0d03ecbc905972eed028705f427269bbe80d2a
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-06-18 09:12:15 -0400

    Added a requirement check calculation that includes expiration

--- src/CareTogether.Core/Engines/PolicyEvaluation/SharedCalculations.cs
+++ src/CareTogether.Core/Engines/PolicyEvaluation/SharedCalculations.cs
@@ -7,6 +7,35 @@ namespace CareTogether.Engines.PolicyEvaluation
 {
     internal static class SharedCalculations
     {
+        public sealed record RequirementCheckResult(bool IsMetOrExempted, DateTime? ExpiresAtUtc);
+
+        internal static RequirementCheckResult RequirementMetOrExempted(string requirementName,
+            DateTime? policySupersededAtUtc, DateTime utcNow,
+            ImmutableList<CompletedRequirementInfoWithExpiration> completedRequirementsWithExpiration,
+            ImmutableList<ExemptedRequirementInfo> exemptedRequirements)
+        {
+            var bestCompletion = completedRequirementsWithExpiration
+                .Where(completed =>
+                    completed.RequirementName == requirementName &&
+                    (policySupersededAtUtc == null || completed.CompletedAtUtc < policySupersededAtUtc) &&
+                    completed.ExpiresAtUtc > utcNow)
+                .MaxBy(completed => completed.ExpiresAtUtc ?? DateTime.MaxValue);
+
+            if (bestCompletion != null)
+                return new RequirementCheckResult(true, bestCompletion.ExpiresAtUtc);
+
+            var bestExemption = exemptedRequirements
+                .Where(exempted =>
+                    exempted.RequirementName == requirementName &&
+                    (exempted.ExemptionExpiresAtUtc == null || exempted.ExemptionExpiresAtUtc > utcNow))
+                .MaxBy(exempted => exempted.ExemptionExpiresAtUtc ?? DateTime.MaxValue);
+
+            if (bestExemption != null)
+                return new RequirementCheckResult(true, bestExemption.ExemptionExpiresAtUtc);
+
+            return new RequirementCheckResult(false, null);
+        }
+
         internal static bool RequirementMetOrExempted(string requirementName,
             DateTime? policySupersededAtUtc, DateTime utcNow,
             ImmutableList<CompletedRequirementInfo> completedRequirements,
