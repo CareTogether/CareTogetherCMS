ad76db63f7ab96eef17f938f4e9c19ae0f256d3d
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-06-18 10:48:06 -0400

    Updated base requirement-met calculation to incorporate expiration logic

--- src/CareTogether.Core/Engines/PolicyEvaluation/SharedCalculations.cs
+++ src/CareTogether.Core/Engines/PolicyEvaluation/SharedCalculations.cs
@@ -18,15 +18,33 @@ namespace CareTogether.Engines.PolicyEvaluation
             };
         }
 
-        internal static bool RequirementMetOrExempted(string requirementName,
+        public sealed record RequirementCheckResult(bool IsMetOrExempted, DateTime? ExpiresAtUtc);
+
+        internal static RequirementCheckResult RequirementMetOrExempted(string requirementName,
             DateTime? policySupersededAtUtc, DateTime utcNow,
             ImmutableList<CompletedRequirementInfo> completedRequirements,
-            ImmutableList<ExemptedRequirementInfo> exemptedRequirements) =>
-            completedRequirements.Any(completed =>
-                completed.RequirementName == requirementName &&
-                (policySupersededAtUtc == null || completed.CompletedAtUtc < policySupersededAtUtc)) ||
-            exemptedRequirements.Any(exempted =>
-                exempted.RequirementName == requirementName &&
-                (exempted.ExemptionExpiresAtUtc == null || exempted.ExemptionExpiresAtUtc > utcNow));
+            ImmutableList<ExemptedRequirementInfo> exemptedRequirements)
+        {
+            var bestCompletion = completedRequirements
+                .Where(completed =>
+                    completed.RequirementName == requirementName &&
+                    (policySupersededAtUtc == null || completed.CompletedAtUtc < policySupersededAtUtc) &&
+                    completed.ExpiresAtUtc > utcNow)
+                .MaxBy(completed => completed.ExpiresAtUtc ?? DateTime.MaxValue);
+
+            if (bestCompletion != null)
+                return new RequirementCheckResult(true, bestCompletion.ExpiresAtUtc);
+
+            var bestExemption = exemptedRequirements
+                .Where(exempted =>
+                    exempted.RequirementName == requirementName &&
+                    (exempted.ExemptionExpiresAtUtc == null || exempted.ExemptionExpiresAtUtc > utcNow))
+                .MaxBy(exempted => exempted.ExemptionExpiresAtUtc ?? DateTime.MaxValue);
+
+            if (bestExemption != null)
+                return new RequirementCheckResult(true, bestExemption.ExemptionExpiresAtUtc);
+
+            return new RequirementCheckResult(false, null);
+        }
     }
 }
