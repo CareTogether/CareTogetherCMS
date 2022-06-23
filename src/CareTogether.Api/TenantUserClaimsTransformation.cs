89f8e498f975096e68703ee6238b3d26667e9c9e
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-06-22 21:20:18 -0400

    Code cleanup

--- src/CareTogether.Api/TenantUserClaimsTransformation.cs
+++ src/CareTogether.Api/TenantUserClaimsTransformation.cs
@@ -20,59 +20,52 @@ namespace CareTogether.Api
 
         public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
         {
-            try
-            {
-                var tenantUserIdentity = new ClaimsIdentity();
-                tenantUserIdentity.Label = "Tenant User";
-                var userId = principal.UserId();
+            var tenantUserIdentity = new ClaimsIdentity();
+            tenantUserIdentity.Label = "Tenant User";
+            var userId = principal.UserId();
 
-                // Look up the tenant access for the user.
-                var userTenantAccess = await accountsResource.GetUserTenantAccessSummaryAsync(userId);
+            // Look up the tenant access for the user.
+            var userTenantAccess = await accountsResource.GetUserTenantAccessSummaryAsync(userId);
 
-                var organizationId = userTenantAccess.OrganizationId;
-                principal.AddClaimOnlyOnce(tenantUserIdentity, Claims.OrganizationId, organizationId.ToString());
-                principal.AddIdentity(tenantUserIdentity);
+            var organizationId = userTenantAccess.OrganizationId;
+            principal.AddClaimOnlyOnce(tenantUserIdentity, Claims.OrganizationId, organizationId.ToString());
+            principal.AddIdentity(tenantUserIdentity);
 
-                // Then look up the organization-managed role access and person ID for that user.
-                var configuration = await policiesResource.GetConfigurationAsync(organizationId);
+            // Then look up the organization-managed role access and person ID for that user.
+            var configuration = await policiesResource.GetConfigurationAsync(organizationId);
 
-                var userAccessConfiguration = configuration.Users[userId];
-                //TODO: There is an inconsistency here -- we are storing/controlling location access both from the
-                //      IAccountsResource *and* the IPoliciesResource.
-                //      This is a further argument that this method should really just call a method on the
-                //      IAuthorizationEngine to obtain the full map of the user's organization/location access,
-                //      roles, and permissions, and simply convert that information into ClaimsIdentity form here.
-                var locationUserIdentities = userAccessConfiguration.LocationRoles
-                    .Select(locationRole =>
+            var userAccessConfiguration = configuration.Users[userId];
+            //TODO: There is an inconsistency here -- we are storing/controlling location access both from the
+            //      IAccountsResource *and* the IPoliciesResource.
+            //      This is a further argument that this method should really just call a method on the
+            //      IAuthorizationEngine to obtain the full map of the user's organization/location access,
+            //      roles, and permissions, and simply convert that information into ClaimsIdentity form here.
+            var locationUserIdentities = userAccessConfiguration.LocationRoles
+                .Select(locationRole =>
+                {
+                    var locationUserIdentity = new ClaimsIdentity($"{organizationId}:{locationRole.LocationId}");
+                    locationUserIdentity.Label = "User Location Access";
+
+                    var locationClaims = new Claim[]
                     {
-                        var locationUserIdentity = new ClaimsIdentity($"{organizationId}:{locationRole.LocationId}");
-                        locationUserIdentity.Label = "User Location Access";
+                        new(Claims.LocationId, locationRole.LocationId.ToString()),
+                        new(tenantUserIdentity.RoleClaimType, locationRole.RoleName),
+                        new(Claims.PersonId, userAccessConfiguration.PersonId.ToString())
+                    };
+                    locationUserIdentity.AddClaims(locationClaims);
 
-                        var locationClaims = new Claim[]
-                        {
-                            new(Claims.LocationId, locationRole.LocationId.ToString()),
-                            new(tenantUserIdentity.RoleClaimType, locationRole.RoleName),
-                            new(Claims.PersonId, userAccessConfiguration.PersonId.ToString())
-                        };
-                        locationUserIdentity.AddClaims(locationClaims);
+                    var rolePermissions = configuration.Roles
+                        .Single(role => role.RoleName == locationRole.RoleName)
+                        .Permissions;
+                    var permissionClaims = rolePermissions
+                        .Select(rolePermission => new Claim(Claims.Permission, rolePermission.ToString()));
+                    locationUserIdentity.AddClaims(permissionClaims);
 
-                        var rolePermissions = configuration.Roles
-                            .Single(role => role.RoleName == locationRole.RoleName)
-                            .Permissions;
-                        var permissionClaims = rolePermissions
-                            .Select(rolePermission => new Claim(Claims.Permission, rolePermission.ToString()));
-                        locationUserIdentity.AddClaims(permissionClaims);
+                    return locationUserIdentity;
+                });
+            principal.AddIdentities(locationUserIdentities);
 
-                        return locationUserIdentity;
-                    });
-                principal.AddIdentities(locationUserIdentities);
-
-                return principal;
-            }
-            catch (System.Exception ex)
-            {
-                throw ex;
-            }
+            return principal;
         }
     }
 }
