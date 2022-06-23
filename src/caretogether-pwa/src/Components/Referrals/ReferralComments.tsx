2991de73613d20dd0b75c64e50b9eca3c1dfe2c4
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-06-23 00:02:43 -0400

    ReferralComments permission guards

--- src/caretogether-pwa/src/Components/Referrals/ReferralComments.tsx
+++ src/caretogether-pwa/src/Components/Referrals/ReferralComments.tsx
@@ -1,6 +1,7 @@
 import { TextField } from "@mui/material";
-import { CombinedFamilyInfo } from "../../GeneratedClient";
+import { CombinedFamilyInfo, Permission } from "../../GeneratedClient";
 import { useReferralsModel } from "../../Model/ReferralsModel";
+import { usePermissions } from "../../Model/SessionModel";
 import { useInlineEditor } from "../../useInlineEditor";
 
 type ReferralCommentsProps = {
@@ -12,28 +13,32 @@ export function ReferralComments({ partneringFamily, referralId }: ReferralComme
   const savedValue = partneringFamily.partneringFamilyInfo?.openReferral?.comments;
 
   const referralsModel = useReferralsModel();
+  const permissions = usePermissions();
 
   const editor = useInlineEditor(async value => {
     await referralsModel.updateReferralComments(partneringFamily.family!.id!, referralId, value);
   }, savedValue);
 
-  return (
-    <>
-      <h3 style={{ marginBottom: 0 }}>
-        Comments
-        {editor.editButton}
-        {editor.cancelButton}
-        {editor.saveButton}
-      </h3>
-      {editor.editing
-        ? <TextField
-            id="referral-comments"
-            helperText="Referral comments are visible to everyone."
-            placeholder="Space for any general notes about the referral, upcoming plans, etc."
-            multiline fullWidth variant="outlined" minRows={2} size="medium"
-            value={editor.value}
-            onChange={e => editor.setValue(e.target.value)} />
-        : savedValue}
-    </>
-  );
+  return permissions(Permission.ViewReferralComments)
+    ? <>
+        <h3 style={{ marginBottom: 0 }}>
+          Comments
+          {permissions(Permission.EditReferral) &&
+            <>
+              {editor.editButton}
+              {editor.cancelButton}
+              {editor.saveButton}
+            </>}
+        </h3>
+        {editor.editing && permissions(Permission.EditReferral)
+          ? <TextField
+              id="referral-comments"
+              helperText="Referral comments are visible to everyone."
+              placeholder="Space for any general notes about the referral, upcoming plans, etc."
+              multiline fullWidth variant="outlined" minRows={2} size="medium"
+              value={editor.value}
+              onChange={e => editor.setValue(e.target.value)} />
+          : savedValue}
+      </>
+    : <></>;
 }
