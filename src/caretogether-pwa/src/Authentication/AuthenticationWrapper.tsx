cd699144b134322367cd79ff381bec30fc8dae20
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-08-09 00:17:38 -0400

    Cleaner separation of responsibilities in AuthenticationWrapper

--- src/caretogether-pwa/src/Authentication/AuthenticationWrapper.tsx
+++ src/caretogether-pwa/src/Authentication/AuthenticationWrapper.tsx
@@ -2,22 +2,13 @@ import React from 'react';
 import { InteractionType } from "@azure/msal-browser";
 import { useMsalAuthentication, useIsAuthenticated } from '@azure/msal-react';
 
-interface InnerAuthWrapperProps {
-  children?: React.ReactNode
-}
-function InnerAuthWrapper({ children }: InnerAuthWrapperProps) {
+function SignInScreen() {
   // Force the user to sign in if not already authenticated, then render the app.
   // See https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/hooks.md
-  useMsalAuthentication(InteractionType.Redirect);
-  const isAuthenticated = useIsAuthenticated();
-  
+  useMsalAuthentication(InteractionType.Redirect); /*, {} as RedirectRequest*/
+
   return (
-    <>
-      {isAuthenticated
-        ? children
-        : <p>You are not signed in. You can try to refresh your page (F5) to reattempt signing in.</p>
-      }
-    </>
+    <p>You are not signed in. You can try to refresh your page (F5) to reattempt signing in.</p>
   );
 }
 
@@ -25,9 +16,13 @@ interface AuthenticationWrapperProps {
   children?: React.ReactNode
 }
 export default function AuthenticationWrapper({ children }: AuthenticationWrapperProps) {
+  const isAuthenticated = useIsAuthenticated();
+
   return (
-    <InnerAuthWrapper>
-      {children}
-    </InnerAuthWrapper>
+    <>
+      {isAuthenticated
+        ? children
+        : <SignInScreen />}
+    </>
   );
 }
