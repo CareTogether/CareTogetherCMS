9c89634d2d6628e5fb8770708cd675778bd81245
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-08-09 01:12:07 -0400

    Code cleanup for ShellSideNavigation (merging in old MainDrawer code)

--- src/caretogether-pwa/src/Shell/ShellSideNavigation.tsx
+++ src/caretogether-pwa/src/Shell/ShellSideNavigation.tsx
@@ -1,4 +1,23 @@
-import { Drawer, Stack, useTheme } from '@mui/material';
+import { Drawer, List, Stack, useTheme } from '@mui/material';
+import PermPhoneMsgIcon from '@mui/icons-material/PermPhoneMsg';
+import PeopleIcon from '@mui/icons-material/People';
+import { ListItemLink } from '../ListItemLink';
+import { useFeatureFlags } from '../Model/ConfigurationModel';
+import { Copyright } from '../Copyright';
+
+function SideNavigationMenu() {
+  const flags = useFeatureFlags();
+
+  return (
+    //  <List aria-label="main navigation">
+    //    <ListItemLink to="/dashboard" primary="Dashboard" icon={<DashboardIcon />} />
+    //  </List>
+    <List aria-label="secondary navigation">
+      {flags.viewReferrals && <ListItemLink to="/referrals" primary="Referrals" icon={<PermPhoneMsgIcon />} />}
+      <ListItemLink to="/volunteers" primary="Volunteers" icon={<PeopleIcon />} />
+    </List>
+  );
+}
 
 interface ShellSideNavigationProps {
   open: boolean;
@@ -6,6 +25,30 @@ interface ShellSideNavigationProps {
 }
 export function ShellSideNavigation({ open, width }: ShellSideNavigationProps) {
   const theme = useTheme();
+  
+  // const drawerPaperOpenStyle = {
+  //   position: 'relative',
+  //   whiteSpace: 'nowrap',
+  //   width: 200,
+  //   transition: theme.transitions.create('width', {
+  //     easing: theme.transitions.easing.sharp,
+  //     duration: theme.transitions.duration.enteringScreen
+  //   })
+  // };
+  
+  // const drawerPaperCloseStyle = {
+  //   position: 'relative',
+  //   whiteSpace: 'nowrap',
+  //   overflowX: 'hidden',
+  //   width: theme.spacing(7),
+  //   [theme.breakpoints.up('sm')]: {
+  //     width: theme.spacing(9),
+  //   },
+  //   transition: theme.transitions.create('width', {
+  //     easing: theme.transitions.easing.sharp,
+  //     duration: theme.transitions.duration.leavingScreen,
+  //   })
+  // };
 
   return (
     <Drawer variant='permanent' open={open}
@@ -21,7 +64,10 @@ export function ShellSideNavigation({ open, width }: ShellSideNavigationProps) {
         }
       }}>
       <Stack sx={{ width: width, paddingTop: { xs: 7, sm: 8, md: 6 } }}>
-        <p>Hello!</p>
+        <SideNavigationMenu />
+        <div style={{ position: 'fixed', bottom: 0, marginLeft: 10}}>
+          <Copyright />
+        </div>
       </Stack>
     </Drawer>
   );
