4fe0d5b242381763454ba777d06a061ff811788f
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-09-03 16:55:33 -0400

    Basic unit test updates for authorization logic

--- test/CareTogether.Core.Test/AuthorizationEngineTests/AuthorizeUserAccess.cs
+++ test/CareTogether.Core.Test/AuthorizationEngineTests/AuthorizeUserAccess.cs
@@ -26,12 +26,13 @@ namespace CareTogether.Core.Test.AuthorizationEngineTests
         static readonly Guid guid4 = Id('4');
         static readonly Guid guid5 = Id('5');
 
-        //TODO: This approach to testing authorization is no longer valid.
-        private static ClaimsPrincipal UserFromPermissions(Guid personId, params Permission[] permissions) =>
+        private static ClaimsPrincipal PersonUserWithRoles(Guid personId, params string[] roles) =>
             new(new ClaimsIdentity(
                 new Claim[] { new Claim(Claims.PersonId.ToString(), personId.ToString()) }
-                    .Concat(permissions.Select(permission => new Claim(Claims.Permission, permission.ToString()))),
+                    .Concat(roles.Select(role => new Claim(ClaimsIdentity.DefaultRoleClaimType, role.ToString()))),
                 $"{guid1}:{guid2}"));
+
+        private static Permission[] allPermissions = Enum.GetValues<Permission>();
 
         private AuthorizationEngine? dut;
 
@@ -70,80 +71,104 @@ namespace CareTogether.Core.Test.AuthorizationEngineTests
         }
 
         [DataTestMethod]
-        [DataRow('0', true, true, true, true, true, true)]
-        [DataRow('1', true, true, true, true, true, true)]
-        [DataRow('2', true, true, true, true, true, true)]
-        [DataRow('3', true, true, true, true, true, true)]
-        [DataRow('4', true, true, true, true, true, true)]
-        [DataRow('5', true, true, true, true, true, true)]
-        [DataRow('6', true, true, true, true, true, true)]
-        [DataRow('7', true, true, true, true, true, true)]
-        [DataRow('8', true, true, true, true, true, true)]
-        [DataRow('9', true, true, true, true, true, true)]
-        [DataRow('a', true, true, true, true, true, true)]
-        [DataRow('b', true, true, true, true, true, true)]
-        [DataRow('c', true, true, true, true, true, true)]
-        [DataRow('d', true, true, true, true, true, true)]
-        [DataRow('e', true, true, true, true, true, true)]
-        [DataRow('f', true, true, true, true, true, true)]
-        public async Task TestAnyPersonWithViewAllFamiliesPermission(char personId,
-            bool expected0, bool expected1, bool expected2, bool expected3, bool expected4, bool expected5)
+        [DataRow('0', 0, 0, 0, 0, 0, 0)]
+        [DataRow('1', 0, 0, 0, 0, 0, 0)]
+        [DataRow('2', 0, 0, 0, 0, 0, 0)]
+        [DataRow('3', 0, 0, 0, 0, 0, 0)]
+        [DataRow('4', 0, 0, 0, 0, 0, 0)]
+        [DataRow('5', 0, 0, 0, 0, 0, 0)]
+        [DataRow('6', 0, 0, 0, 0, 0, 0)]
+        [DataRow('7', 0, 0, 0, 0, 0, 0)]
+        [DataRow('8', 0, 0, 0, 0, 0, 0)]
+        [DataRow('9', 0, 0, 0, 0, 0, 0)]
+        [DataRow('a', 0, 0, 0, 0, 0, 0)]
+        [DataRow('b', 0, 0, 0, 0, 0, 0)]
+        [DataRow('c', 0, 0, 0, 0, 0, 0)]
+        [DataRow('d', 0, 0, 0, 0, 0, 0)]
+        [DataRow('e', 0, 0, 0, 0, 0, 0)]
+        [DataRow('f', 0, 0, 0, 0, 0, 0)]
+        public async Task TestAccessWithNoRoles(char personId,
+            int expected0, int expected1, int expected2, int expected3, int expected4, int expected5)
         {
-            var user = UserFromPermissions(Id(personId), Permission.ReadFamilyDocuments);
+            var user = PersonUserWithRoles(Id(personId));
             await EvaluateAccess(user, personId, expected0, expected1, expected2, expected3, expected4, expected5);
         }
 
         [DataTestMethod]
-        [DataRow('0', true, false, false, false, false, false)]
-        [DataRow('1', false, true, false, false, false, false)]
-        [DataRow('2', false, true, false, false, false, false)]
-        [DataRow('3', false, true, false, false, false, false)]
-        [DataRow('4', false, false, false, false, true, false)]
-        [DataRow('5', false, false, true, false, false, false)]
-        [DataRow('6', false, false, true, false, false, false)]
-        [DataRow('7', false, false, true, false, false, false)]
-        [DataRow('8', false, false, false, true, false, false)]
-        [DataRow('9', false, false, false, true, false, false)]
-        [DataRow('a', false, false, true, false, false, false)]
-        [DataRow('b', false, false, false, false, false, true)]
-        [DataRow('c', false, false, false, false, false, true)]
-        [DataRow('d', false, false, false, false, false, false)]
-        [DataRow('e', false, false, false, false, false, false)]
-        [DataRow('f', false, false, false, false, false, false)]
-        public async Task TestPeopleWithoutAdditionalViewPermissionsCanSeeOnlyTheirOwnFamily(char personId,
-            bool expected0, bool expected1, bool expected2, bool expected3, bool expected4, bool expected5)
+        [DataRow('0', -1, -1, -1, -1, -1, -1)]
+        [DataRow('1', -1, -1, -1, -1, -1, -1)]
+        [DataRow('2', -1, -1, -1, -1, -1, -1)]
+        [DataRow('3', -1, -1, -1, -1, -1, -1)]
+        [DataRow('4', -1, -1, -1, -1, -1, -1)]
+        [DataRow('5', -1, -1, -1, -1, -1, -1)]
+        [DataRow('6', -1, -1, -1, -1, -1, -1)]
+        [DataRow('7', -1, -1, -1, -1, -1, -1)]
+        [DataRow('8', -1, -1, -1, -1, -1, -1)]
+        [DataRow('9', -1, -1, -1, -1, -1, -1)]
+        [DataRow('a', -1, -1, -1, -1, -1, -1)]
+        [DataRow('b', -1, -1, -1, -1, -1, -1)]
+        [DataRow('c', -1, -1, -1, -1, -1, -1)]
+        [DataRow('d', -1, -1, -1, -1, -1, -1)]
+        [DataRow('e', -1, -1, -1, -1, -1, -1)]
+        [DataRow('f', -1, -1, -1, -1, -1, -1)]
+        public async Task TestAccessWithOrganizationAdministratorRole(char personId,
+            int expected0, int expected1, int expected2, int expected3, int expected4, int expected5)
         {
-            var user = UserFromPermissions(Id(personId));
+            var user = PersonUserWithRoles(Id(personId), "OrganizationAdministrator");
             await EvaluateAccess(user, personId, expected0, expected1, expected2, expected3, expected4, expected5);
         }
 
         [DataTestMethod]
-        [DataRow('0', true, false, false, false, false, false)]
-        [DataRow('1', false, true, true, true, true, false)]
-        [DataRow('2', false, true, true, true, true, false)]
-        [DataRow('3', false, true, true, true, true, false)]
-        [DataRow('4', false, true, true, true, true, false)]
-        [DataRow('5', false, true, true, true, true, false)]
-        [DataRow('6', false, true, true, true, true, false)]
-        [DataRow('7', false, true, true, true, true, false)]
-        [DataRow('8', false, true, true, true, true, false)]
-        [DataRow('9', false, true, true, true, true, false)]
-        [DataRow('a', false, true, true, true, true, false)]
-        [DataRow('b', false, false, false, false, false, true)]
-        [DataRow('c', false, false, false, false, false, true)]
-        [DataRow('d', false, false, false, false, false, false)]
-        [DataRow('e', false, false, false, false, false, false)]
-        [DataRow('f', false, false, false, false, false, false)]
-        public async Task TestLinkedFamiliesPermissionGrantsAccessToAllFamiliesConnectedViaReferralAssignment(char personId,
-            bool expected0, bool expected1, bool expected2, bool expected3, bool expected4, bool expected5)
+        [DataRow('0', 5, 2, 2, 2, 2, 2)]
+        [DataRow('1', 2, 5, 2, 2, 2, 2)]
+        [DataRow('2', 2, 5, 2, 2, 2, 2)]
+        [DataRow('3', 2, 5, 2, 2, 2, 2)]
+        [DataRow('4', 2, 10, 3, 3, 5, 2)]
+        [DataRow('5', 2, 10, 5, 3, 3, 2)]
+        [DataRow('6', 2, 10, 5, 3, 3, 2)]
+        [DataRow('7', 2, 10, 5, 3, 3, 2)]
+        [DataRow('8', 2, 2, 3, 5, 3, 2)]
+        [DataRow('9', 2, 2, 3, 5, 3, 2)]
+        [DataRow('a', 2, 10, 5, 3, 3, 2)]
+        [DataRow('b', 2, 2, 2, 2, 2, 5)]
+        [DataRow('c', 2, 2, 2, 2, 2, 5)]
+        [DataRow('d', 2, 2, 2, 2, 2, 2)]
+        [DataRow('e', 2, 2, 2, 2, 2, 2)]
+        [DataRow('f', 2, 2, 2, 2, 2, 2)]
+        public async Task TestAccessWithVolunteerRole(char personId,
+            int expected0, int expected1, int expected2, int expected3, int expected4, int expected5)
         {
-            var user = UserFromPermissions(Id(personId), Permission.ReadFamilyDocuments);
+            var user = PersonUserWithRoles(Id(personId), "Volunteer");
+            await EvaluateAccess(user, personId, expected0, expected1, expected2, expected3, expected4, expected5);
+        }
+
+        [DataTestMethod]
+        [DataRow('0', -1, -1, -1, -1, -1, -1)]
+        [DataRow('1', -1, -1, -1, -1, -1, -1)]
+        [DataRow('2', -1, -1, -1, -1, -1, -1)]
+        [DataRow('3', -1, -1, -1, -1, -1, -1)]
+        [DataRow('4', -1, -1, -1, -1, -1, -1)]
+        [DataRow('5', -1, -1, -1, -1, -1, -1)]
+        [DataRow('6', -1, -1, -1, -1, -1, -1)]
+        [DataRow('7', -1, -1, -1, -1, -1, -1)]
+        [DataRow('8', -1, -1, -1, -1, -1, -1)]
+        [DataRow('9', -1, -1, -1, -1, -1, -1)]
+        [DataRow('a', -1, -1, -1, -1, -1, -1)]
+        [DataRow('b', -1, -1, -1, -1, -1, -1)]
+        [DataRow('c', -1, -1, -1, -1, -1, -1)]
+        [DataRow('d', -1, -1, -1, -1, -1, -1)]
+        [DataRow('e', -1, -1, -1, -1, -1, -1)]
+        [DataRow('f', -1, -1, -1, -1, -1, -1)]
+        public async Task TestAccessWithOrganizationAdministratorAndVolunteerRoles(char personId,
+            int expected0, int expected1, int expected2, int expected3, int expected4, int expected5)
+        {
+            var user = PersonUserWithRoles(Id(personId), "OrganizationAdministrator", "Volunteer");
             await EvaluateAccess(user, personId, expected0, expected1, expected2, expected3, expected4, expected5);
         }
 
 
         private async Task EvaluateAccess(ClaimsPrincipal user, char personId,
-            bool expected0, bool expected1, bool expected2, bool expected3, bool expected4, bool expected5)
+            int expected0, int expected1, int expected2, int expected3, int expected4, int expected5)
         {
             var result0 = await dut!.AuthorizeUserAccessAsync(guid1, guid2, user, new FamilyAuthorizationContext(guid0));
             var result1 = await dut!.AuthorizeUserAccessAsync(guid1, guid2, user, new FamilyAuthorizationContext(guid1));
@@ -152,12 +177,14 @@ namespace CareTogether.Core.Test.AuthorizationEngineTests
             var result4 = await dut!.AuthorizeUserAccessAsync(guid1, guid2, user, new FamilyAuthorizationContext(guid4));
             var result5 = await dut!.AuthorizeUserAccessAsync(guid1, guid2, user, new FamilyAuthorizationContext(guid5));
 
-            Assert.AreEqual(expected0, result0, $"Person '{personId}' access to family '0' expected {expected0} but was {result0}");
-            Assert.AreEqual(expected1, result1, $"Person '{personId}' access to family '1' expected {expected1} but was {result1}");
-            Assert.AreEqual(expected2, result2, $"Person '{personId}' access to family '2' expected {expected2} but was {result2}");
-            Assert.AreEqual(expected3, result3, $"Person '{personId}' access to family '3' expected {expected3} but was {result3}");
-            Assert.AreEqual(expected4, result4, $"Person '{personId}' access to family '4' expected {expected4} but was {result4}");
-            Assert.AreEqual(expected5, result5, $"Person '{personId}' access to family '5' expected {expected5} but was {result5}");
+            int X(int expected) => expected == -1 ? allPermissions.Length : expected;
+
+            Assert.AreEqual(X(expected0), result0.Count, $"Person '{personId}' access to family '0' expected {X(expected0)} but was {result0.Count}");
+            Assert.AreEqual(X(expected1), result1.Count, $"Person '{personId}' access to family '1' expected {X(expected1)} but was {result1.Count}");
+            Assert.AreEqual(X(expected2), result2.Count, $"Person '{personId}' access to family '2' expected {X(expected2)} but was {result2.Count}");
+            Assert.AreEqual(X(expected3), result3.Count, $"Person '{personId}' access to family '3' expected {X(expected3)} but was {result3.Count}");
+            Assert.AreEqual(X(expected4), result4.Count, $"Person '{personId}' access to family '4' expected {X(expected4)} but was {result4.Count}");
+            Assert.AreEqual(X(expected5), result5.Count, $"Person '{personId}' access to family '5' expected {X(expected5)} but was {result5.Count}");
         }
     }
 }
