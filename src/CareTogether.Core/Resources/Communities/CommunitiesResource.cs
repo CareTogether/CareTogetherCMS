943feda76c1e73e961a932cf38e9da5f305dfcad
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-02-03 12:25:06 -0500

    CommunitiesResource backend design & implementation

---
+++ src/CareTogether.Core/Resources/Communities/CommunitiesResource.cs
@@ -0,0 +1,43 @@
+﻿using CareTogether.Utilities.EventLog;
+using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace CareTogether.Resources.Communities
+{
+    public sealed class CommunitiesResource : ICommunitiesResource
+    {
+        private readonly IEventLog<CommunityCommandExecutedEvent> communitiesEventLog;
+        private readonly ConcurrentLockingStore<(Guid organizationId, Guid locationId), CommunitiesModel> tenantCommunitiesModels;
+
+
+        public CommunitiesResource(IEventLog<CommunityCommandExecutedEvent> communitiesEventLog)
+        {
+            this.communitiesEventLog = communitiesEventLog;
+            tenantCommunitiesModels = new ConcurrentLockingStore<(Guid organizationId, Guid locationId), CommunitiesModel>(key =>
+                CommunitiesModel.InitializeAsync(communitiesEventLog.GetAllEventsAsync(key.organizationId, key.locationId)));
+        }
+
+
+        public async Task<Community> ExecuteCommunityCommandAsync(Guid organizationId, Guid locationId, CommunityCommand command,
+            Guid userId)
+        {
+            using (var lockedModel = await tenantCommunitiesModels.WriteLockItemAsync((organizationId, locationId)))
+            {
+                var result = lockedModel.Value.ExecuteCommunityCommand(command, userId, DateTime.UtcNow);
+
+                await communitiesEventLog.AppendEventAsync(organizationId, locationId, result.Event, result.SequenceNumber);
+                result.OnCommit();
+                return result.Community;
+            }
+        }
+
+        public async Task<ImmutableList<Community>> ListLocationCommunitiesAsync(Guid organizationId, Guid locationId)
+        {
+            using (var lockedModel = await tenantCommunitiesModels.ReadLockItemAsync((organizationId, locationId)))
+            {
+                return lockedModel.Value.FindCommunities(c => true);
+            }
+        }
+    }
+}
---
+++ src/CareTogether.Core/Resources/Communities/ICommunitiesResource.cs
@@ -0,0 +1,44 @@
+﻿using JsonPolymorph;
+using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace CareTogether.Resources.Communities
+{
+    public sealed record Community(Guid Id, string Name, string Description,
+        ImmutableList<Guid> MemberFamilies, ImmutableList<CommunityRoleAssignment> CommunityRoleAssignments,
+        ImmutableList<UploadedDocumentInfo> UploadedDocuments);
+    public sealed record CommunityRoleAssignment(Guid PersonId, string CommunityRole);
+
+    [JsonHierarchyBase]
+    public abstract partial record CommunityCommand(Guid CommunityId);
+    public sealed record CreateCommunity(Guid CommunityId, string Name, string Description)
+        : CommunityCommand(CommunityId);
+    public sealed record RenameCommunity(Guid CommunityId, string Name)
+        : CommunityCommand(CommunityId);
+    public sealed record EditCommunityDescription(Guid CommunityId, string Description)
+        : CommunityCommand(CommunityId);
+    public sealed record AddCommunityMemberFamily(Guid CommunityId, Guid FamilyId)
+        : CommunityCommand(CommunityId);
+    public sealed record RemoveCommunityMemberFamily(Guid CommunityId, Guid FamilyId)
+        : CommunityCommand(CommunityId);
+    public sealed record AddCommunityRoleAssignment(Guid CommunityId, Guid PersonId, string CommunityRole)
+        : CommunityCommand(CommunityId);
+    public sealed record RemoveCommunityRoleAssignment(Guid CommunityId, Guid PersonId, string CommunityRole)
+        : CommunityCommand(CommunityId);
+    public sealed record UploadCommunityDocument(Guid CommunityId, Guid UploadedDocumentId, string UploadedFileName)
+        : CommunityCommand(CommunityId);
+    public sealed record DeleteUploadedCommunityDocument(Guid CommunityId, Guid UploadedDocumentId)
+        : CommunityCommand(CommunityId);
+
+    /// <summary>
+    /// The <see cref="ICommunitiesResource"/> is responsible for all communities in CareTogether.
+    /// This includes family-level membership and community role assignments as well as tracking community content.
+    /// </summary>
+    public interface ICommunitiesResource
+    {
+        Task<Community> ExecuteCommunityCommandAsync(Guid organizationId, Guid locationId, CommunityCommand command, Guid userId);
+
+        Task<ImmutableList<Community>> ListLocationCommunitiesAsync(Guid organizationId, Guid locationId);
+    }
+}
