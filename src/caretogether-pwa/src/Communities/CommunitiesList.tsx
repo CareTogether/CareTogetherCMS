d4a0773f5ee8a24a543cf43c49a0e2c741a2a9a1
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-03-12 18:01:29 -0400

    CommunitiesList cleanup & Add button draft

--- src/caretogether-pwa/src/Communities/CommunitiesList.tsx
+++ src/caretogether-pwa/src/Communities/CommunitiesList.tsx
@@ -1,10 +1,11 @@
-import { Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Fab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { Community } from '../GeneratedClient';
 import { useLoadable } from '../Hooks/useLoadable';
 import { useDataInitialized, visibleCommunitiesQuery } from '../Model/DirectoryModel';
 import { ProgressBackdrop } from '../Shell/ProgressBackdrop';
 import useScreenTitle from '../Shell/ShellScreenTitle';
+import AddIcon from '@mui/icons-material/Add';
 
 export function CommunitiesList() {
   useScreenTitle("Communities");
@@ -22,48 +23,52 @@ export function CommunitiesList() {
 
   return (!dataInitialized
     ? <ProgressBackdrop>
-      <p>Loading communities...</p>
-    </ProgressBackdrop>
-    : <Container maxWidth={false} sx={{ paddingLeft: '12px' }}>
-      <TableContainer>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              <TableCell align='left' sx={{ minWidth: 200 }}>
-                Name
-              </TableCell>
-              <TableCell align='left' sx={{ minWidth: 400 }}>
-                Description
-              </TableCell>
-              <TableCell align='right' sx={{ minWidth: 50 }}>
-                Member Families
-              </TableCell>
-              <TableCell align='right' sx={{ minWidth: 50 }}>
-                Role Assigments
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {communities.map(community => <TableRow key={community.id}
-              hover role='listitem' tabIndex={-1}
-              sx={{ cursor: 'pointer' }}
-              onClick={() => openCommunity(community)}>
-              <TableCell align='left' sx={{ minWidth: 200 }}>
-                {community.name}
-              </TableCell>
-              <TableCell align='left' sx={{ minWidth: 400 }}>
-                {community.description}
-              </TableCell>
-              <TableCell align='right' sx={{ minWidth: 50 }}>
-                {community.memberFamilies?.length}
-              </TableCell>
-              <TableCell align='right' sx={{ minWidth: 50 }}>
-                {community.communityRoleAssignments?.length}
-              </TableCell>
-            </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Container>);
+        <p>Loading communities...</p>
+      </ProgressBackdrop>
+    : <>
+        <TableContainer>
+          <Table aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell align='left' sx={{ minWidth: 200 }}>
+                  Name
+                </TableCell>
+                <TableCell align='left' sx={{ minWidth: 400 }}>
+                  Description
+                </TableCell>
+                <TableCell align='right' sx={{ minWidth: 50 }}>
+                  Member Families
+                </TableCell>
+                <TableCell align='right' sx={{ minWidth: 50 }}>
+                  Role Assigments
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {communities.map(community => <TableRow key={community.id}
+                hover role='listitem' tabIndex={-1}
+                sx={{ cursor: 'pointer' }}
+                onClick={() => openCommunity(community)}>
+                <TableCell align='left' sx={{ minWidth: 200 }}>
+                  {community.name}
+                </TableCell>
+                <TableCell align='left' sx={{ minWidth: 400 }}>
+                  {community.description}
+                </TableCell>
+                <TableCell align='right' sx={{ minWidth: 50 }}>
+                  {community.memberFamilies?.length}
+                </TableCell>
+                <TableCell align='right' sx={{ minWidth: 50 }}>
+                  {community.communityRoleAssignments?.length}
+                </TableCell>
+              </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Fab color="primary" aria-label="add"
+          sx={{position: 'fixed', right: '30px', bottom: '70px'}}>
+          <AddIcon />
+        </Fab>
+      </>);
 }
