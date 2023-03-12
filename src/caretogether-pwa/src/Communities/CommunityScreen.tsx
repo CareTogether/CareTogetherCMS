d1675bf0b7b50da3b7cf8fbcbc9a3b05aea49528
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-03-12 19:31:55 -0400

    Draft layout of CommunityScreen

--- src/caretogether-pwa/src/Communities/CommunityScreen.tsx
+++ src/caretogether-pwa/src/Communities/CommunityScreen.tsx
@@ -1,8 +1,24 @@
-import { Container, Toolbar } from '@mui/material';
+import { Container, Grid, List, ListItem, ListItemText, Toolbar, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
-import { useCommunityLookup, useDataInitialized } from '../Model/DirectoryModel';
+import { useRecoilValue } from 'recoil';
+import { familyNameString } from '../Families/FamilyName';
+import { personNameString } from '../Families/PersonName';
+import { CombinedFamilyInfo, Community } from '../GeneratedClient';
+import { useCommunityLookup, useDataInitialized, usePersonAndFamilyLookup, visibleFamiliesQuery } from '../Model/DirectoryModel';
 import { ProgressBackdrop } from '../Shell/ProgressBackdrop';
 import useScreenTitle from '../Shell/ShellScreenTitle';
+
+interface CommunityDescriptionProps {
+  community: Community
+}
+function CommunityDescription({ community }: CommunityDescriptionProps) {
+  return (
+    <>
+      <Typography variant='caption'>Description</Typography>
+      <p>{community.description}</p>
+    </>
+  );
+}
 
 export function CommunityScreen() {
   const communityIdMaybe = useParams<{ communityId: string; }>();
@@ -12,6 +28,17 @@ export function CommunityScreen() {
 
   const communityLookup = useCommunityLookup();
   const community = communityLookup(communityId)!;
+
+  const personLookup = usePersonAndFamilyLookup();
+  const assignees = (community?.communityRoleAssignments || []).map(assignee => ({
+    personAndFamily: personLookup(assignee.personId),
+    communityRole: assignee.communityRole
+  }));
+
+  const visibleFamilies = useRecoilValue(visibleFamiliesQuery);
+
+  const memberFamilies = (community?.memberFamilies || []).map(familyId =>
+    visibleFamilies.find(family => family.family?.id === familyId)).filter(family => family) as CombinedFamilyInfo[];
   
   useScreenTitle(community?.name || "...");
 
@@ -21,14 +48,38 @@ export function CommunityScreen() {
   // const isWideScreen = useMediaQuery(theme.breakpoints.up('xl'));
   // const permissions = useCommunityPermissions(community);
   
-  return (!dataInitialized
+  return ((!dataInitialized || !community)
     ? <ProgressBackdrop>
         <p>Loading community...</p>
       </ProgressBackdrop>
-    :
-    <Container maxWidth={false} sx={{ paddingLeft: '12px' }}>
-      <Toolbar variant="dense" disableGutters={true}>
-      </Toolbar>
-      <pre>{communityId}</pre>
-    </Container>);
+    : <Container maxWidth={false} sx={{ paddingLeft: '12px' }}>
+        <Toolbar variant="dense" disableGutters={true}>
+        </Toolbar>
+        <Grid container>
+          <Grid item xs={12}>
+            <CommunityDescription community={community} />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <Typography variant='h6'>Role Assignments</Typography>
+            <List>
+              {assignees.map(assignee =>
+                <ListItem key={`${assignee.personAndFamily!.person!.id}-${assignee.communityRole}`} disablePadding>
+                  <ListItemText
+                    primary={personNameString(assignee.personAndFamily.person)}
+                    secondary={assignee.communityRole} />
+                </ListItem>)}
+            </List>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <Typography variant='h6'>Member Families</Typography>
+            <List>
+              {memberFamilies.map(family =>
+                <ListItem key={family.family!.id} disablePadding>
+                  <ListItemText
+                    primary={familyNameString(family)} />
+                </ListItem>)}
+            </List>
+          </Grid>
+        </Grid>
+      </Container>);
 }
