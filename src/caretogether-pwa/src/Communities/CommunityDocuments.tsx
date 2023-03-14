80c3ae6ca19aff092187296bae2ffb6dcb0fa4f8
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-03-14 00:00:48 -0400

    Document downloads

--- src/caretogether-pwa/src/Communities/CommunityDocuments.tsx
+++ src/caretogether-pwa/src/Communities/CommunityDocuments.tsx
@@ -2,11 +2,13 @@ import { IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText 
 import { personNameString } from '../Families/PersonName';
 import { CommunityInfo, DeleteUploadedCommunityDocument, Permission, UploadedDocumentInfo } from '../GeneratedClient';
 import { useCommunityCommand, useUserLookup } from '../Model/DirectoryModel';
-import { useCommunityPermissions } from '../Model/SessionModel';
+import { currentLocationState, currentOrganizationState, useCommunityPermissions } from '../Model/SessionModel';
 import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { format } from 'date-fns';
 import { useBackdrop } from '../Hooks/useBackdrop';
+import { downloadCommunityFile } from '../Model/FilesModel';
+import { useRecoilValue } from 'recoil';
 
 interface CommunityDocumentsProps {
   communityInfo: CommunityInfo;
@@ -14,6 +16,9 @@ interface CommunityDocumentsProps {
 export function CommunityDocuments({ communityInfo }: CommunityDocumentsProps) {
   const permissions = useCommunityPermissions(communityInfo);
   const community = communityInfo.community!;
+  
+  const organizationId = useRecoilValue(currentOrganizationState);
+  const locationId = useRecoilValue(currentLocationState);
 
   const userLookup = useUserLookup();
   const documents = (community.uploadedDocuments || []).map(document => ({
@@ -38,6 +43,12 @@ export function CommunityDocuments({ communityInfo }: CommunityDocumentsProps) {
     }
   }
 
+  async function downloadDocument(document: UploadedDocumentInfo) {
+    await withBackdrop(async () => {
+      await downloadCommunityFile(organizationId, locationId, community.id!, document.uploadedDocumentId!);
+    });
+  }
+
   return <List sx={{ '& .MuiListItemIcon-root': { minWidth: 36 } }}>
     {documents.map(doc => <ListItem key={doc.document.uploadedDocumentId} disablePadding
       secondaryAction={permissions(Permission.DeleteCommunityDocuments)
@@ -47,7 +58,8 @@ export function CommunityDocuments({ communityInfo }: CommunityDocumentsProps) {
         </IconButton>
         : null}>
       {permissions(Permission.ReadCommunityDocuments)
-        ? <ListItemButton disableGutters sx={{ paddingTop: 0, paddingBottom: 0 }}>
+        ? <ListItemButton disableGutters sx={{ paddingTop: 0, paddingBottom: 0 }}
+            onClick={() => downloadDocument(doc.document)}>
             <ListItemIcon>
               <InsertDriveFileOutlinedIcon />
             </ListItemIcon>
