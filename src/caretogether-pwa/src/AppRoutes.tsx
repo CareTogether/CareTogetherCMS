4b4da6aaf182c9115673bd4f83a14ac230a47735
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-04-09 21:46:35 -0400

    Route error screen

--- src/caretogether-pwa/src/AppRoutes.tsx
+++ src/caretogether-pwa/src/AppRoutes.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Dashboard } from "./Dashboard";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Referrals } from "./Referrals/Referrals";
 import { Volunteers } from "./Volunteers/Volunteers";
 import { Settings } from "./Settings/Settings";
 import { FamilyScreen } from "./Families/FamilyScreen";
 import { Communities } from "./Communities/Communities";
 import { UserProfile } from "./UserProfile/UserProfile";
+
+function RouteError(): React.ReactElement {
+  throw new Error(`The URL path '${window.location.href}' is invalid.`);
+}
 
 export function AppRoutes() { //TODO: Merge this with Shell components from index.ts for clearer integration?
   return (
@@ -20,9 +24,9 @@ export function AppRoutes() { //TODO: Merge this with Shell components from inde
         <Route path="settings/*" element={<Settings />} />
       </Route>
       <Route path="me/*" element={<UserProfile />} />
-      <Route path="*" element={<Navigate to="/" replace />} /*
-        TODO: Error page (via exception?) -- but first, if it's a valid old path (pre-org+loc path) then
-              try redirecting based on the user's first available organization and location. */ />
+      <Route path="*" element={<RouteError />}
+      /* TODO: If it's a valid old path (pre-org+loc path) then
+               try redirecting based on the user's first available organization and location. */ />
     </Routes>
   );
 }
