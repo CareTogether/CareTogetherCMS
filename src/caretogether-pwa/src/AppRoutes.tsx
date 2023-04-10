91ed5861baeb2acbbad815a5d41988014be157f3
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-04-09 21:40:56 -0400

    Conceptual draft of client-side route changes

--- src/caretogether-pwa/src/AppRoutes.tsx
+++ src/caretogether-pwa/src/AppRoutes.tsx
@@ -8,17 +8,21 @@ import { FamilyScreen } from "./Families/FamilyScreen";
 import { Communities } from "./Communities/Communities";
 import { UserProfile } from "./UserProfile/UserProfile";
 
-export function AppRoutes() {
+export function AppRoutes() { //TODO: Merge this with Shell components from index.ts for clearer integration?
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="families/:familyId" element={<FamilyScreen />} />
-      <Route path="referrals/*" element={<Referrals />} />
-      <Route path="volunteers/*" element={<Volunteers />} />
-      <Route path="communities/*" element={<Communities />} />
-      <Route path="settings/*" element={<Settings />} />
+      <Route path="/:organizationId/:locationId" /*TODO: Include a layout route under this for the shell*/>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="families/:familyId" element={<FamilyScreen />} />
+        <Route path="referrals/*" element={<Referrals />} />
+        <Route path="volunteers/*" element={<Volunteers />} />
+        <Route path="communities/*" element={<Communities />} />
+        <Route path="settings/*" element={<Settings />} />
+      </Route>
       <Route path="me/*" element={<UserProfile />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="*" element={<Navigate to="/" replace />} /*
+        TODO: Error page (via exception?) -- but first, if it's a valid old path (pre-org+loc path) then
+              try redirecting based on the user's first available organization and location. */ />
     </Routes>
   );
 }
