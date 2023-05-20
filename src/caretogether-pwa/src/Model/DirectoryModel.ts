5f0b0733fbd264594fea449003afb473ee577074
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-05-19 21:35:56 -0400

    Addressing compiler warning

--- src/caretogether-pwa/src/Model/DirectoryModel.ts
+++ src/caretogether-pwa/src/Model/DirectoryModel.ts
@@ -207,7 +207,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonName = usePersonCommandCallback(
-    async (familyId, personId: string, firstName: string, lastName: string) => {
+    async (_familyId, personId: string, firstName: string, lastName: string) => {
       const command = new UpdatePersonName({
         personId: personId
       });
@@ -216,7 +216,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonGender = usePersonCommandCallback(
-    async (familyId, personId: string, gender: Gender) => {
+    async (_familyId, personId: string, gender: Gender) => {
       const command = new UpdatePersonGender({
         personId: personId
       });
@@ -224,7 +224,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonAge = usePersonCommandCallback(
-    async (familyId, personId: string, age: Age) => {
+    async (_familyId, personId: string, age: Age) => {
       const command = new UpdatePersonAge({
         personId: personId
       });
@@ -232,7 +232,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonEthnicity = usePersonCommandCallback(
-    async (familyId, personId: string, ethnicity: string) => {
+    async (_familyId, personId: string, ethnicity: string) => {
       const command = new UpdatePersonEthnicity({
         personId: personId
       });
@@ -240,7 +240,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonConcerns = usePersonCommandCallback(
-    async (familyId, personId: string, concerns: string | null) => {
+    async (_familyId, personId: string, concerns: string | null) => {
       const command = new UpdatePersonConcerns({
         personId: personId
       });
@@ -248,7 +248,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonNotes = usePersonCommandCallback(
-    async (familyId, personId: string, notes: string | null) => {
+    async (_familyId, personId: string, notes: string | null) => {
       const command = new UpdatePersonNotes({
         personId: personId
       });
@@ -256,14 +256,14 @@ export function useDirectoryModel() {
       return command;
     });
   const undoCreatePerson = usePersonCommandCallback(
-    async (familyId, personId: string) => {
+    async (_familyId, personId: string) => {
       const command = new UndoCreatePerson({
         personId: personId
       });
       return command;
     });
   const addPersonPhoneNumber = usePersonCommandCallback(
-    async (familyId, personId: string, phoneNumber: string, phoneType: PhoneNumberType, isPreferred: boolean) => {
+    async (_familyId, personId: string, phoneNumber: string, phoneType: PhoneNumberType, isPreferred: boolean) => {
       const command = new AddPersonPhoneNumber({
         personId: personId
       });
@@ -272,7 +272,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonPhoneNumber = usePersonCommandCallback(
-    async (familyId, personId: string, phoneId: string, phoneNumber: string, phoneType: PhoneNumberType, isPreferred: boolean) => {
+    async (_familyId, personId: string, phoneId: string, phoneNumber: string, phoneType: PhoneNumberType, isPreferred: boolean) => {
       const command = new UpdatePersonPhoneNumber({
         personId: personId
       });
@@ -281,7 +281,7 @@ export function useDirectoryModel() {
       return command;
     });
   const addPersonEmailAddress = usePersonCommandCallback(
-    async (familyId, personId: string, emailAddress: string, phoneType: EmailAddressType, isPreferred: boolean) => {
+    async (_familyId, personId: string, emailAddress: string, phoneType: EmailAddressType, isPreferred: boolean) => {
       const command = new AddPersonEmailAddress({
         personId: personId
       });
@@ -290,7 +290,7 @@ export function useDirectoryModel() {
       return command;
     });
   const updatePersonEmailAddress = usePersonCommandCallback(
-    async (familyId, personId: string, phoneId: string, emailAddress: string, phoneType: EmailAddressType, isPreferred: boolean) => {
+    async (_familyId, personId: string, phoneId: string, emailAddress: string, phoneType: EmailAddressType, isPreferred: boolean) => {
       const command = new UpdatePersonEmailAddress({
         personId: personId
       });
