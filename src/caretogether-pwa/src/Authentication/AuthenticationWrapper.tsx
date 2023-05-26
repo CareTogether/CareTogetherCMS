c7fcb69579baf95f03ad3054b3c08e539892327b
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-05-26 14:12:00 -0400

    WIP commit

--- src/caretogether-pwa/src/Authentication/AuthenticationWrapper.tsx
+++ src/caretogether-pwa/src/Authentication/AuthenticationWrapper.tsx
@@ -1,13 +1,62 @@
 import React, { useEffect } from 'react';
-import { InteractionType } from "@azure/msal-browser";
-import { useMsalAuthentication, useAccount, AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
+import { globalMsalInstance } from './Auth';
 import { ProgressBackdrop } from '../Shell/ProgressBackdrop';
 import { useScopedTrace } from '../Hooks/useScopedTrace';
 import { useSearchParams } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValueLoadable, useSetRecoilState } from 'recoil';
 import { userIdState } from "../Model/Data";
 
 function SignIn() {
+  const trace = useScopedTrace("SignIn");
+  
+  // Ensure that the 'state' parameter is always round-tripped through MSAL.
+  // This is useful, e.g., for person invite redemption which may require interrupting a
+  // non-authenticated user with an authentication redirect before they can complete the
+  // invite redemption process.
+  const [searchParams, ] = useSearchParams();
+  const stateQueryParam = searchParams.get("state");
+  trace(`state: ${stateQueryParam}`);
+  
+  // Ensure that the authenticated user is set as the default/active account.
+  // One the user is authenticated, store the user's account for future reference.
+  // This becomes the root of the Recoil dataflow graph.
+  //const setAllAccounts = useSetRecoilState(allAccountsState);
+  //TODO: Handle token/session expiration to intercept the automatic redirect and prompt the user first?
+  //TODO: Smoother handling of deeplink routing (integrating with React Router)?
+  //TODO: Incorporate new AAD B2C refresh token support?
+  const setUserId = useSetRecoilState(userIdState);
+  useEffect(() => {
+    trace(`starting loginRedirect with state: ${stateQueryParam}`);
+    globalMsalInstance.loginRedirect({
+      scopes: [import.meta.env.VITE_APP_AUTH_SCOPES],
+      state: stateQueryParam ?? undefined
+    }).then(() => {
+      trace(`loginRedirect completed`);
+
+      const allAccounts = globalMsalInstance.getAllAccounts();
+      trace(`allAccounts length: ${allAccounts.length}`);
+      
+      let activeAccount = globalMsalInstance.getActiveAccount();
+      trace(`activeAccount: ${activeAccount?.localAccountId}`);
+  
+      if (!activeAccount) {
+        if (allAccounts.length === 1) {
+          trace("Marking the sole signed-in account as active");
+          let activeAccount = allAccounts[0];
+          globalMsalInstance.setActiveAccount(activeAccount);
+        } else if (allAccounts.length === 0) {
+          throw new Error("Your user was unexpectedly not authenticated; please report this as a bug.");
+        } else {
+          //TODO: Handle account selection when multiple accounts are signed in (this is an edge case)
+          throw new Error("You are signed in with more than one user account. Try clearing your browser cache and cookies, and report this as a bug if the issue persists.");
+        }
+      }
+      
+      trace(`Setting the model to use the active account's user ID: ${activeAccount!.localAccountId}`);
+      setUserId(activeAccount!.localAccountId);
+    });
+  }, [setUserId]);
+
   return (
     <ProgressBackdrop opaque>
       <p>Signing in...</p>
@@ -17,33 +66,32 @@ function SignIn() {
 }
 
 function AuthenticatedUserWrapper({ children }: React.PropsWithChildren) {
-  const trace = useScopedTrace("SelectedUserWrapper");
-
-  const defaultAccount = useAccount();
-  const msal = useMsal();
-  
-  const allAccounts = msal.accounts;
-
-  trace(`defaultAccount: ${defaultAccount?.localAccountId}`);
-  trace(`MSAL accounts: ${JSON.stringify(allAccounts.map(account => account.localAccountId))}`);
-
+  //const trace = useScopedTrace("SelectedUserWrapper");
+/*
+  // Ensure that the authenticated user is set as the default/active account.
   // One the user is authenticated, store the user's account for future reference.
   // This becomes the root of the Recoil dataflow graph.
-  const setUserId = useSetRecoilState(userIdState);
   useEffect(() => {
-    if (defaultAccount) {
-      trace(`Setting the model to use the active account's user ID: ${defaultAccount.localAccountId}`);
-      setUserId(defaultAccount.localAccountId);
+    const allAccounts = globalMsalInstance.getAllAccounts();
+    trace(`MSAL accounts: ${JSON.stringify(allAccounts.map(account => account.localAccountId))}`);
+    
+    const activeAccount = globalMsalInstance.getActiveAccount();
+    trace(`activeAccount: ${activeAccount?.localAccountId}`);
+
+    if (activeAccount) {
+      trace(`Setting the model to use the active account's user ID: ${activeAccount.localAccountId}`);
+      setUserId(activeAccount.localAccountId);
     } else if (allAccounts.length === 1) {
       trace("Marking the signed-in account as active");
-      msal.instance.setActiveAccount(allAccounts[0]);
+      globalMsalInstance.setActiveAccount(allAccounts[0]);
     } else if (allAccounts.length === 0) {
       throw new Error("Your user was unexpectedly not authenticated; please report this as a bug.");
     } else {
+      //TODO: Handle account selection when multiple accounts are signed in (this is an edge case)
       throw new Error("You are signed in with more than one user account. Try clearing your browser cache and cookies, and report this as a bug if the issue persists.");
     }
-  }, [allAccounts, defaultAccount, setUserId, trace, msal.instance]);
-
+  }, [setUserId, trace]);
+*/
   return (
     <>
       {children}
@@ -52,38 +100,17 @@ function AuthenticatedUserWrapper({ children }: React.PropsWithChildren) {
 }
 
 export default function AuthenticationWrapper({ children }: React.PropsWithChildren) {
-  //TODO: Long-term, consider removing the msal-react package altogether as it just doesn't play nicely with Recoil state updates.
   const trace = useScopedTrace("AuthenticationWrapper");
-
-  // Ensure that the 'state' parameter is always round-tripped through MSAL.
-  // This is useful, e.g., for person invite redemption which may require interrupting a
-  // non-authenticated user with an authentication redirect before they can complete the
-  // invite redemption process.
-  const [searchParams, ] = useSearchParams();
-  const stateQueryParam = searchParams.get("state");
-  trace(`state: ${stateQueryParam}`);
-
-  // Force the user to sign in if not already authenticated.
-  // See https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/hooks.md
-  //TODO: Handle token/session expiration to intercept the automatic redirect and prompt the user first?
-  //TODO: Smoother handling of deeplink routing (integrating with React Router)?
-  //TODO: Incorporate new AAD B2C refresh token support?
-  useMsalAuthentication(InteractionType.Redirect, {
-    scopes: [import.meta.env.VITE_APP_AUTH_SCOPES],
-    state: stateQueryParam ?? undefined
-  });
   
-  return (
-    <>
-      <AuthenticatedTemplate>
-        {/*TODO: Handle account selection when multiple accounts are signed in (this is an edge case)*/}
-        <AuthenticatedUserWrapper>
-          {children}
-        </AuthenticatedUserWrapper>
-      </AuthenticatedTemplate>
-      <UnauthenticatedTemplate>
-        <SignIn />
-      </UnauthenticatedTemplate>
-    </>
+  //TODO: Add an event callback to be notified if the user's session expires?
+  // Force the user to sign in if not already authenticated.
+  const userId = useRecoilValueLoadable(userIdState);
+  trace(`userId: ${userId}`);
+
+  return (userId
+    ? <AuthenticatedUserWrapper>
+        {children}
+      </AuthenticatedUserWrapper>
+    : <SignIn />
   );
 }
