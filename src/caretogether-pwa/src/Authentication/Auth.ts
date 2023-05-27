a16ec3616662585d1b82beb8dc68d8606e5116cf
Lars Kemmann <lars.kemmann@bynalogic.com>
2023-05-26 21:54:32 -0400

    WIP commit 3

--- src/caretogether-pwa/src/Authentication/Auth.ts
+++ src/caretogether-pwa/src/Authentication/Auth.ts
@@ -1,4 +1,6 @@
-import { PublicClientApplication, IPublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication, IPublicClientApplication, InteractionRequiredAuthError, AuthenticationResult } from "@azure/msal-browser";
+import { AtomEffect, atom } from "recoil";
+import { loggingEffect } from "../Utilities/loggingEffect";
 
 // MSAL configuration for single page application authorization. For guidance, see
 // https://docs.microsoft.com/en-us/azure/active-directory/develop/scenario-spa-app-configuration?tabs=react and
@@ -16,3 +18,135 @@ const config = {
 };
 
 export const globalMsalInstance: IPublicClientApplication = new PublicClientApplication(config);
+
+function trace(scope: string, message: string) {
+  console.debug(`[${scope}] ${message}`);
+  //TODO: Invoke App Insights as well.
+}
+
+function renderMsalError(error: unknown) {
+  return `${error}`; //TODO: How to log MSAL.js errors?
+}
+
+function displayableError(error: Error | unknown) {
+  return error; //TODO: Include a user-friendly message that's compatible with the global error boundary.
+}
+
+function withDefaultScopes(scopes: string[]) {
+  // Add the default OpenID Connect scopes and then deduplicate the resulting entries.
+  return [...new Set(scopes.concat([
+    "openid",
+    "profile",
+    "offline_access"
+  ]))];
+}
+const scopes = withDefaultScopes([import.meta.env.VITE_APP_AUTH_SCOPES]);
+
+async function loginAndSetActiveAccountAsync(): Promise<string> {
+  // The ultimate objective of this function is to obtain an AuthenticationResult from Azure AD via MSAL.js
+  // and return the local account ID of the authenticated account.
+  let result: AuthenticationResult | null = null;
+
+  // Step 1: Initialize the MSAL.js library
+  // https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/initialization.md
+  trace(`Login`, `Initializing MSAL...`);
+  await globalMsalInstance.initialize();
+
+  // Step 2: Since we are using redirect-based interaction, check on each page load whether the
+  //         browser has just completed a redirect flow and handle the response from Azure AD if so.
+  trace(`Login`, `Checking for a redirect response to process...`);
+  result = await globalMsalInstance.handleRedirectPromise();
+  trace(`Login`, `Redirect result account: ${result?.account?.localAccountId}`);
+
+  // Step 3: Look for any accounts that are already considered to be signed in by MSAL.js.
+  //         Note that these accounts may or may not have a valid session with Azure AD.
+  trace(`Login`, `Loading authenticated accounts...`);
+  const allAccounts = globalMsalInstance.getAllAccounts();
+  trace(`Login`, `MSAL accounts: ${JSON.stringify(allAccounts.map(account => account.localAccountId))}`);
+
+  if (allAccounts.length === 0) {
+    // Step 4a: If zero accounts were found, attempt a silent SSO to try to use the user's single
+    //          active session with Azure AD (if the user has exactly one such session) to authenticate.
+    try {
+      trace(`Login`, `Attempting silent SSO...`);
+      result = await globalMsalInstance.ssoSilent({
+        scopes: scopes
+      });
+      trace(`Login`, `Silent SSO was successful.`);
+    } catch (error) {
+      trace(`Login`, `Silent SSO failed with: ${renderMsalError(error)}`);
+      if (!(error instanceof InteractionRequiredAuthError)) {
+        throw displayableError(error);
+      }
+    }
+  } else {
+    // Step 4b: If one or more accounts was found, set the active account (if needed) and attempt
+    //          to acquire a token silently to verify the user's authentication.
+    if (allAccounts.length > 1) {
+      //NOTE: Based on the documentation, we can assume that if only one account is found then MSAL will
+      //      guarantee that it is *always* the active account.
+      const firstAccount = allAccounts[0];
+      trace(`Login`, `Setting active account to first known account: ${firstAccount.localAccountId}`);
+      globalMsalInstance.setActiveAccount(firstAccount);
+    }
+    const activeAccount = globalMsalInstance.getActiveAccount();
+    trace(`Login`, `Active account is: ${activeAccount?.localAccountId}`);
+    try {
+      trace(`Login`, `Attempting silent token acquisition using the active account '${activeAccount?.localAccountId}'...`);
+      result = await globalMsalInstance.acquireTokenSilent({
+        scopes: scopes
+      });
+      trace(`Login`, `Silent token acquisition was successful.`);
+    } catch (error) {
+      trace(`Login`, `Silent token acquisition failed with: ${renderMsalError(error)}`);
+      if (!(error instanceof InteractionRequiredAuthError)) {
+        throw displayableError(error);
+      }
+    }
+  }
+
+  // Step 5: One the user is authenticated, store the user's account ID for future reference.
+  //         This becomes the root of the Recoil dataflow graph.
+  if (result && result.account) {
+    return result.account.localAccountId;
+  } else if (result && !result.account) {
+    throw displayableError(
+      new Error("An authentication result was obtained but no account was determined to be signed in."));
+  }
+
+  // Step 6: If the user needs to authenticate interactively, trigger a redirect flow.
+  try {
+    trace(`Login`, `Attempting token acquisition with a redirect because interaction is required...`);
+    const stateQueryParam = new URLSearchParams(window.location.search).get("state");
+    await globalMsalInstance.acquireTokenRedirect({
+      scopes: scopes,
+      // Ensure that the 'state' parameter is always round-tripped through MSAL.
+      // This is useful, e.g., for person invite redemption which may require interrupting a
+      // non-authenticated user with an authentication redirect before they can complete the
+      // invite redemption process.
+      state: stateQueryParam ?? undefined
+    });
+    throw displayableError(
+      new Error("This code was expected to be unreachable since acquireTokenRedirect should never actually return."));
+  } catch (error) {
+    trace(`Login`, `Token acquisition with redirect failed with: ${renderMsalError(error)}`);
+    throw displayableError(error);
+  }
+}
+
+//TODO: Add an event callback to be notified if the user's session expires?
+//      NO -- idiomatic MSAL.js usage is to stick with these 'acquireToken*' flows and then alert the user if interaction is needed.
+
+const initializeUserIdStateAsync: AtomEffect<any> = params => {
+  trace("InitializeUserIdStateAsync", params.node.key);
+  params.setSelf(loginAndSetActiveAccountAsync());
+}
+
+// This will be set by AuthenticationWrapper once the user has authenticated and the default account is set.
+export const userIdState = atom<string>({
+  key: 'userIdState',
+  effects: [
+    loggingEffect,
+    initializeUserIdStateAsync
+  ]
+});
--- src/caretogether-pwa/src/Authentication/AuthenticationWrapper.tsx
+++ src/caretogether-pwa/src/Authentication/AuthenticationWrapper.tsx
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { globalMsalInstance } from './Auth';
 import { ProgressBackdrop } from '../Shell/ProgressBackdrop';
 import { useScopedTrace } from '../Hooks/useScopedTrace';
 import { useSearchParams } from 'react-router-dom';
 import { useRecoilValueLoadable, useSetRecoilState } from 'recoil';
-import { userIdState } from "../Model/Data";
+import { userIdState } from "../Authentication/Auth";
 import { useLoadable } from '../Hooks/useLoadable';
 
 function SignIn() {
   const trace = useScopedTrace("SignIn");
   
-  // Ensure that the 'state' parameter is always round-tripped through MSAL.
-  // This is useful, e.g., for person invite redemption which may require interrupting a
-  // non-authenticated user with an authentication redirect before they can complete the
-  // invite redemption process.
   const [searchParams, ] = useSearchParams();
   const stateQueryParam = searchParams.get("state");
   trace(`state: ${stateQueryParam}`);
@@ -72,39 +68,17 @@ function SignIn() {
 
   return (
     <ProgressBackdrop opaque>
-      <p>Signing in...</p>
-      <p>If you are not redirected within a few seconds,<br /> try refreshing this page.</p>
     </ProgressBackdrop>
   );
 }
 
 function AuthenticatedUserWrapper({ children }: React.PropsWithChildren) {
-  //const trace = useScopedTrace("SelectedUserWrapper");
-/*
-  // Ensure that the authenticated user is set as the default/active account.
-  // One the user is authenticated, store the user's account for future reference.
-  // This becomes the root of the Recoil dataflow graph.
-  useEffect(() => {
-    const allAccounts = globalMsalInstance.getAllAccounts();
-    trace(`MSAL accounts: ${JSON.stringify(allAccounts.map(account => account.localAccountId))}`);
-    
-    const activeAccount = globalMsalInstance.getActiveAccount();
-    trace(`activeAccount: ${activeAccount?.localAccountId}`);
+  const trace = useScopedTrace("AuthenticatedUserWrapper");
 
-    if (activeAccount) {
-      trace(`Setting the model to use the active account's user ID: ${activeAccount.localAccountId}`);
-      setUserId(activeAccount.localAccountId);
-    } else if (allAccounts.length === 1) {
-      trace("Marking the signed-in account as active");
-      globalMsalInstance.setActiveAccount(allAccounts[0]);
-    } else if (allAccounts.length === 0) {
-      throw new Error("Your user was unexpectedly not authenticated; please report this as a bug.");
-    } else {
-      //TODO: Handle account selection when multiple accounts are signed in (this is an edge case)
-      throw new Error("You are signed in with more than one user account. Try clearing your browser cache and cookies, and report this as a bug if the issue persists.");
-    }
-  }, [setUserId, trace]);
-*/
+  // This will suspend until a user ID has been set by the `userIdState` initialization logic.
+  const userId = useLoadable(userIdState);
+  trace(`userId: ${userId}`);
+  
   return (
     <>
       {children}
@@ -113,32 +87,15 @@ function AuthenticatedUserWrapper({ children }: React.PropsWithChildren) {
 }
 
 export default function AuthenticationWrapper({ children }: React.PropsWithChildren) {
-  const trace = useScopedTrace("AuthenticationWrapper");
-
-  const [msalInitialized, setMsalInitialized] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      trace("Initializing MSAL...");
-      await globalMsalInstance.initialize();
-      trace("MSAL initialization completed.");
-      setMsalInitialized(true);
-    })();
-  }, [setMsalInitialized]);
-  
-  //TODO: Add an event callback to be notified if the user's session expires?
-  // Force the user to sign in if not already authenticated.
-  const userId = useLoadable(userIdState);
-  trace(`userId: ${userId}`);
-
-  return (!msalInitialized
-    ? <ProgressBackdrop opaque>
-        <p>Initializing security...</p>
-      </ProgressBackdrop>
-    : !userId
-    ? <SignIn />
-    : <AuthenticatedUserWrapper>
+  return (
+    <Suspense fallback={
+      <ProgressBackdrop opaque>
+        <p>Signing in...</p>
+        <p>If you are not redirected within a few seconds,<br /> try refreshing this page.</p>
+      </ProgressBackdrop>}>
+      <AuthenticatedUserWrapper>
         {children}
       </AuthenticatedUserWrapper>
+    </Suspense>
   );
 }
