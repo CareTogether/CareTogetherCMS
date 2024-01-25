de843db781d0a20a5f03ba7d235e09b3b6e815ed
Lars Kemmann <lars.kemmann@bynalogic.com>
2024-01-25 16:58:44 -0500

    Added missing Timelines namespace reference

--- test/CareTogether.Core.Test/ReferralCalculationTests/CalculateMissingMonitoringRequirementsWithinCompletionGap.cs
+++ test/CareTogether.Core.Test/ReferralCalculationTests/CalculateMissingMonitoringRequirementsWithinCompletionGap.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Immutable;
+using Timelines;
 
 namespace CareTogether.Core.Test.ReferralCalculationTests
 {
