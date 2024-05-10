08583c101cf0b2e853e797a45b6d40b569ef7130
dschaefer <donald.e.schaefer@gmail.com>
2024-05-09 22:43:25 -0400

    Updates to resolve PR feedback

--- src/caretogether-pwa/src/Families/FamilyScreen.tsx
+++ src/caretogether-pwa/src/Families/FamilyScreen.tsx
@@ -1,10 +1,11 @@
-import { Container, Toolbar, Grid, Button, useMediaQuery, useTheme, Box, IconButton, ListItemText, Menu, MenuItem, MenuList, Chip, Divider, Card, CardHeader } from '@mui/material';
+import { Container, Toolbar, Grid, Button, useMediaQuery, useTheme, Box, IconButton, ListItemText, Menu, MenuItem, MenuList, Chip, Divider, Card, CardHeader, ListItemButton, ListItemIcon } from '@mui/material';
 import { Arrangement, ArrangementPolicy, CompletedCustomFieldInfo, Permission, ReferralCloseReason, RoleRemovalReason } from '../GeneratedClient';
 import { useRecoilValue } from 'recoil';
 import { useParams } from 'react-router';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
 import { AdultCard } from './AdultCard';
 import { ChildCard } from './ChildCard';
 import { useState } from 'react';
@@ -261,7 +262,7 @@ export function FamilyScreen() {
             <ActivityTimeline family={family} />
           </Grid>
         </Grid>
-        <Grid item xs={12} md={8}>
+        <Grid item xs={12} md={8}>		
           <Grid container spacing={0}>
             <Grid item xs={12} md={4}>
               <PrimaryContactEditor family={family} />
@@ -334,6 +335,25 @@ export function FamilyScreen() {
                   onClose={() => setOpenNewReferralDialogOpen(false)} />)}
             </Grid>
           </Grid>
+		<Grid container spacing={0} sx={{ marginBottom: '1rem' }}>			
+			<Grid item xs={12}>
+              <h3 style={{ marginBottom: 0 }}>Communities</h3>
+				{familyCommunityInfo?.map(communityInfo => {
+					return (
+						<ListItemButton sx={{ padding: '.5rem', border: '1px solid #e0e0e0', borderRadius: '5px' }}
+							onClick={() => communityInfo.community && communityInfo.community.id ? appNavigate.community(communityInfo.community.id) : ({})}>
+							<ListItemIcon sx={{ alignSelf: 'center', justifyContent: 'center' }}>
+								<Diversity3Icon color='primary' />
+							</ListItemIcon>
+							<ListItemText sx={{ alignSelf: 'baseline' }}
+								primary={communityInfo.community?.name}
+								primaryTypographyProps={{ color: theme.palette.primary.main }}
+							></ListItemText>
+						</ListItemButton>
+					);
+				})}
+            </Grid>
+		</Grid>
           {permissions(Permission.ViewReferralComments) && family.partneringFamilyInfo?.openReferral &&
             <Grid container spacing={0}>
               <ReferralComments partneringFamily={family}
@@ -402,23 +422,7 @@ export function FamilyScreen() {
               </Grid>}
           </Grid>
           <Grid container spacing={0}>
-			<Grid item xs={12}>
-              <h3 style={{ marginBottom: 0 }}>Communities</h3>
-				{familyCommunityInfo?.map(communityInfo => {
-					return (
-						<Card variant="outlined" sx={{ paddingBottom: '1rem' }}>
-							<CardHeader 
-								sx={{ paddingBottom: 0 }}
-								title={communityInfo.community?.name}
-								subheader={communityInfo.community?.description}
-								onClick={() => {
-									return communityInfo.community && communityInfo.community.id ? appNavigate.community(communityInfo.community.id) : ({})}
-								}
-							></CardHeader>
-						</Card>
-					);
-				})}
-            </Grid>
+			
             {family.partneringFamilyInfo?.openReferral &&
               <Grid item xs={12}>
                 <div style={{ display: `flex`, justifyContent: `space-between`, maxWidth: `100%`, flexWrap: `wrap` }}>
