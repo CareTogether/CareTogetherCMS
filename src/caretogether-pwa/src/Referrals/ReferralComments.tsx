bf08578f313ed8dad444f3f5d5ee1b01c9dcb46c
Lars Kemmann <lars.kemmann@bynalogic.com>
2024-07-16 00:05:14 -0400

    Bugfix for ReferralComments

--- src/caretogether-pwa/src/Referrals/ReferralComments.tsx
+++ src/caretogether-pwa/src/Referrals/ReferralComments.tsx
@@ -13,35 +13,35 @@ export function ReferralComments({ partneringFamily, referralId }: ReferralComme
   const referralsModel = useReferralsModel();
   const permissions = useFamilyPermissions(partneringFamily);
   const openReferrals: Referral[] = (partneringFamily?.partneringFamilyInfo?.openReferral !== undefined) ? [partneringFamily.partneringFamilyInfo.openReferral] : [];
-  const closedReferrals: Referral[] = (partneringFamily?.partneringFamilyInfo?.closedReferrals === undefined) ? [] : 
-  [...partneringFamily.partneringFamilyInfo.closedReferrals!].sort((r1, r2) => r1.closedAtUtc!.getUTCMilliseconds() - r2.closedAtUtc!.getUTCMilliseconds());
-  const allReferrals: Referral[] = [...openReferrals, ...closedReferrals];    
+  const closedReferrals: Referral[] = (partneringFamily?.partneringFamilyInfo?.closedReferrals === undefined) ? [] :
+    [...partneringFamily.partneringFamilyInfo.closedReferrals!].sort((r1, r2) => r1.closedAtUtc!.getUTCMilliseconds() - r2.closedAtUtc!.getUTCMilliseconds());
+  const allReferrals: Referral[] = [...openReferrals, ...closedReferrals];
   const savedValue = allReferrals.find(r => r!.id == referralId)!.comments;
   const editor = useInlineEditor(async savedValue => {
     await referralsModel.updateReferralComments(partneringFamily.family!.id!, referralId, savedValue);
   }, savedValue);
 
   return permissions(Permission.ViewReferralComments)
-    ? 
-	<Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-		<h3 style={{ margin: 0 }}>
-			Comments
-			{permissions(Permission.EditReferral) &&
-			<>
-				{editor.editButton}
-				{editor.cancelButton}
-				{editor.saveButton}
-			</>}
-		</h3>
-		{editor.editing && permissions(Permission.EditReferral)
-			? <TextField
-			id="referral-comments"
-			helperText="Referral comments are visible to everyone."
-			placeholder="Space for any general notes about the referral, upcoming plans, etc."
-			multiline fullWidth variant="outlined" minRows={2} size="medium"
-			value={savedValue} // TODO: Why isn't this working?
-			onChange={e => editor.setValue(e.target.value)} />
-			: savedValue}
+    ?
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+      <h3 style={{ margin: 0 }}>
+        Comments
+        {permissions(Permission.EditReferral) &&
+          <>
+            {editor.editButton}
+            {editor.cancelButton}
+            {editor.saveButton}
+          </>}
+      </h3>
+      {editor.editing && permissions(Permission.EditReferral)
+        ? <TextField
+          id="referral-comments"
+          helperText="Referral comments are visible to everyone."
+          placeholder="Space for any general notes about the referral, upcoming plans, etc."
+          multiline fullWidth variant="outlined" minRows={2} size="medium"
+          value={editor.value ?? ''}
+          onChange={e => editor.setValue(e.target.value)} />
+        : savedValue}
     </Box>
     : <></>;
 }
