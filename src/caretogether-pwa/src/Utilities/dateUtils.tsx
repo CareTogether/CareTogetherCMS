d68bbad2d640e519ee9faa571dc67eb889a130b8
PabloDinella <pablo.dinella@gmail.com>
2024-12-06 10:52:34 -0300

    Fix two typos that went unnoticed

--- src/caretogether-pwa/src/Utilities/dateUtils.tsx
+++ src/caretogether-pwa/src/Utilities/dateUtils.tsx
@@ -1,5 +1,5 @@
 export function formatUtcDateOnly(input: Date) {
-  // format date only to en-US date format, wihtout time zone offsetting
+  // format date only to en-US date format, without time zone offsetting
   // 2025-11-22T00:00:00.000Z will output 11/22/2025, independently of browser's time zone
   return new Intl.DateTimeFormat('en-US', {
     timeZone: 'UTC',
