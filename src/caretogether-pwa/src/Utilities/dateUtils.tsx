ffb400e832317a4010c373c81f7b6580f6a55234
PabloDinella <pablo.dinella@gmail.com>
2025-02-21 15:26:12 -0300

    Show date for exemption and standardize date format to dd/mm/yy

--- src/caretogether-pwa/src/Utilities/dateUtils.tsx
+++ src/caretogether-pwa/src/Utilities/dateUtils.tsx
@@ -3,6 +3,9 @@ export function formatUtcDateOnly(input: Date) {
   // 2025-11-22T00:00:00.000Z will output 11/22/2025, independently of browser's time zone
   return new Intl.DateTimeFormat('en-US', {
     timeZone: 'UTC',
+    day: '2-digit',
+    month: '2-digit',
+    year: '2-digit',
   }).format(input);
 }
 
