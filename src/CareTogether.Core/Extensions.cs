f8c4c0c64a0fb8fa5f6fe47aee7d62cd0b6fa030
PabloDinella <pablo.dinella@gmail.com>
2025-02-25 16:57:25 -0300

    Update UpdateSingle method to handle null values and add item if not found (upsert)

--- src/CareTogether.Core/Extensions.cs
+++ src/CareTogether.Core/Extensions.cs
@@ -16,9 +16,15 @@ namespace CareTogether
         }
 
         public static ImmutableList<T> UpdateSingle<T>(this ImmutableList<T> list, Func<T, bool> predicate,
-            Func<T, T> selector)
+            Func<T?, T> selector)
         {
-            var oldValue = list.Single(predicate);
+            var oldValue = list.SingleOrDefault(predicate);
+
+            if (oldValue == null)
+            {
+                return list.Add(selector(default));
+            }
+
             var newValue = selector(oldValue);
             return list.Replace(oldValue, newValue);
         }
