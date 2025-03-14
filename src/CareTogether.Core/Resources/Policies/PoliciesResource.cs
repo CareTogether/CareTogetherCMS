abf36f94756417e530110c639d21dbec640ab94c
PabloDinella <pablo.dinella@gmail.com>
2025-03-14 18:05:55 -0300

    Create the tenantModels in PoliciesResource every time, to get latest data

--- src/CareTogether.Core/Resources/Policies/PoliciesResource.cs
+++ src/CareTogether.Core/Resources/Policies/PoliciesResource.cs
@@ -1,11 +1,11 @@
-﻿using CareTogether.Resources.Accounts;
-using CareTogether.Utilities.EventLog;
-using CareTogether.Utilities.ObjectStore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
+using CareTogether.Resources.Accounts;
+using CareTogether.Utilities.EventLog;
+using CareTogether.Utilities.ObjectStore;
 
 namespace CareTogether.Resources.Policies
 {
@@ -15,31 +15,23 @@ namespace CareTogether.Resources.Policies
         private const string POLICY = "policy";
         private const string SECRETS = "secrets";
 
-
         private readonly IObjectStore<OrganizationConfiguration> configurationStore;
         private readonly IObjectStore<EffectiveLocationPolicy> locationPoliciesStore;
         private readonly IObjectStore<OrganizationSecrets> organizationSecretsStore;
         private readonly IEventLog<PersonAccessEvent> personAccessEventLog;
-        private readonly ConcurrentLockingStore<(Guid organizationId, Guid locationId), PersonAccessModel> tenantModels;
-
 
         public PoliciesResource(
             IObjectStore<OrganizationConfiguration> configurationStore,
             IObjectStore<EffectiveLocationPolicy> locationPoliciesStore,
             IObjectStore<OrganizationSecrets> organizationSecretsStore,
-            IEventLog<PersonAccessEvent> personAccessEventLog)
+            IEventLog<PersonAccessEvent> personAccessEventLog
+        )
         {
             this.configurationStore = configurationStore;
             this.locationPoliciesStore = locationPoliciesStore;
             this.organizationSecretsStore = organizationSecretsStore;
             this.personAccessEventLog = personAccessEventLog;
-            
-            tenantModels = new ConcurrentLockingStore<(Guid organizationId, Guid locationId), PersonAccessModel>(async key =>
-            {
-                return await PersonAccessModel.InitializeAsync(personAccessEventLog.GetAllEventsAsync(key.organizationId, key.locationId));
-            });
         }
-
 
         public async Task<OrganizationConfiguration> GetConfigurationAsync(Guid organizationId)
         {
@@ -47,23 +39,22 @@ namespace CareTogether.Resources.Policies
             return Render(result);
         }
 
-        public async Task<OrganizationConfiguration> UpsertRoleDefinitionAsync(Guid organizationId,
-            string roleName, RoleDefinition role)
+        public async Task<OrganizationConfiguration> UpsertRoleDefinitionAsync(
+            Guid organizationId,
+            string roleName,
+            RoleDefinition role
+        )
         {
             if (roleName == SystemConstants.ORGANIZATION_ADMINISTRATOR)
                 throw new InvalidOperationException("The organization administrator role cannot be edited.");
 
             var config = await configurationStore.GetAsync(organizationId, Guid.Empty, CONFIG);
-            var newConfig = config with
-            {
-                Roles = config.Roles.AddOrReplace(r => r.RoleName == roleName, _ => role)
-            };
+            var newConfig = config with { Roles = config.Roles.AddOrReplace(r => r.RoleName == roleName, _ => role) };
             await configurationStore.UpsertAsync(organizationId, Guid.Empty, CONFIG, newConfig);
             return Render(newConfig);
         }
 
-        public async Task<OrganizationConfiguration> DeleteRoleDefinitionAsync(Guid organizationId,
-            string roleName)
+        public async Task<OrganizationConfiguration> DeleteRoleDefinitionAsync(Guid organizationId, string roleName)
         {
             if (roleName == SystemConstants.ORGANIZATION_ADMINISTRATOR)
                 throw new InvalidOperationException("The organization administrator role cannot be removed.");
@@ -74,27 +65,35 @@ namespace CareTogether.Resources.Policies
 
             if (roleToRemove == default)
                 throw new InvalidOperationException($"Role '{roleName}' does not exist.");
-            
+
+            // Create the model every time we need it, to ensure we have the latest data
+            // TODO: In V2, find a better way to deal with this
+            var tenantModels = new ConcurrentLockingStore<(Guid organizationId, Guid locationId), PersonAccessModel>(
+                async key =>
+                {
+                    return await PersonAccessModel.InitializeAsync(
+                        personAccessEventLog.GetAllEventsAsync(key.organizationId, key.locationId)
+                    );
+                }
+            );
+
             // Check if any users have this role assigned
             foreach (var location in config.Locations)
             {
                 using (var lockedModel = await tenantModels.ReadLockItemAsync((organizationId, location.Id)))
                 {
-                    var usersWithRole = lockedModel.Value.FindAccess(entry => 
-                        entry.Roles.Contains(roleName));
-                    
+                    var usersWithRole = lockedModel.Value.FindAccess(entry => entry.Roles.Contains(roleName));
+
                     if (usersWithRole.Any())
                     {
                         throw new InvalidOperationException(
-                            $"Cannot delete role '{roleName}' because it is currently assigned to {usersWithRole.Count} user(s) in location '{location.Name}'.");
+                            $"Cannot delete role '{roleName}' because it is currently assigned to {usersWithRole.Count} user(s) in location '{location.Name}'."
+                        );
                     }
                 }
             }
-            
-            var newConfig = config with
-            {
-                Roles = config.Roles.Remove(roleToRemove)
-            };
+
+            var newConfig = config with { Roles = config.Roles.Remove(roleToRemove) };
 
             await configurationStore.UpsertAsync(organizationId, Guid.Empty, CONFIG, newConfig);
 
@@ -109,27 +108,45 @@ namespace CareTogether.Resources.Policies
             {
                 ReferralPolicy = result.ReferralPolicy with
                 {
-                    ArrangementPolicies = result.ReferralPolicy.ArrangementPolicies
-                        .Select(ap => ap with
-                        {
-                            ArrangementFunctions = ap.ArrangementFunctions
-                                .Select(af => af with
-                                {
-                                    EligibleIndividualVolunteerRoles = af.EligibleIndividualVolunteerRoles ??
-                                        result.ReferralPolicy.FunctionPolicies
-                                            ?.SingleOrDefault(fp => fp.FunctionName == af.FunctionName)
-                                            ?.Eligibility.EligibleIndividualVolunteerRoles ?? [],
-                                    EligibleVolunteerFamilyRoles = af.EligibleVolunteerFamilyRoles ??
-                                        result.ReferralPolicy.FunctionPolicies
-                                            ?.SingleOrDefault(fp => fp.FunctionName == af.FunctionName)
-                                            ?.Eligibility.EligibleVolunteerFamilyRoles ?? [],
-                                    EligiblePeople = af.EligiblePeople ??
-                                        result.ReferralPolicy.FunctionPolicies
-                                            ?.SingleOrDefault(fp => fp.FunctionName == af.FunctionName)
-                                            ?.Eligibility.EligiblePeople ?? []
-                                }).ToImmutableList()
-                        }).ToImmutableList()
-                }
+                    ArrangementPolicies = result
+                        .ReferralPolicy.ArrangementPolicies.Select(ap =>
+                            ap with
+                            {
+                                ArrangementFunctions = ap
+                                    .ArrangementFunctions.Select(af =>
+                                        af with
+                                        {
+                                            EligibleIndividualVolunteerRoles =
+                                                af.EligibleIndividualVolunteerRoles
+                                                ?? result
+                                                    .ReferralPolicy.FunctionPolicies?.SingleOrDefault(fp =>
+                                                        fp.FunctionName == af.FunctionName
+                                                    )
+                                                    ?.Eligibility.EligibleIndividualVolunteerRoles
+                                                ?? [],
+                                            EligibleVolunteerFamilyRoles =
+                                                af.EligibleVolunteerFamilyRoles
+                                                ?? result
+                                                    .ReferralPolicy.FunctionPolicies?.SingleOrDefault(fp =>
+                                                        fp.FunctionName == af.FunctionName
+                                                    )
+                                                    ?.Eligibility.EligibleVolunteerFamilyRoles
+                                                ?? [],
+                                            EligiblePeople =
+                                                af.EligiblePeople
+                                                ?? result
+                                                    .ReferralPolicy.FunctionPolicies?.SingleOrDefault(fp =>
+                                                        fp.FunctionName == af.FunctionName
+                                                    )
+                                                    ?.Eligibility.EligiblePeople
+                                                ?? [],
+                                        }
+                                    )
+                                    .ToImmutableList(),
+                            }
+                        )
+                        .ToImmutableList(),
+                },
             };
 
             return effectivePolicy;
@@ -141,7 +158,6 @@ namespace CareTogether.Resources.Policies
             return result;
         }
 
-
         private OrganizationConfiguration Render(OrganizationConfiguration config) =>
             config with
             {
@@ -149,10 +165,19 @@ namespace CareTogether.Resources.Policies
                 // that always has *all* permissions granted to it. This ensures that administrators never
                 // miss out on a newly defined permission that may not have been explicitly granted to
                 // them in their organization's role configuration.
-                Roles = config.Roles.Insert(0,
-                    new RoleDefinition(SystemConstants.ORGANIZATION_ADMINISTRATOR, IsProtected: true, ImmutableList.Create(
-                        new ContextualPermissionSet(new GlobalPermissionContext(),
-                            Enum.GetValues<Permission>().ToImmutableList()))))
+                Roles = config.Roles.Insert(
+                    0,
+                    new RoleDefinition(
+                        SystemConstants.ORGANIZATION_ADMINISTRATOR,
+                        IsProtected: true,
+                        ImmutableList.Create(
+                            new ContextualPermissionSet(
+                                new GlobalPermissionContext(),
+                                Enum.GetValues<Permission>().ToImmutableList()
+                            )
+                        )
+                    )
+                ),
             };
     }
 }
