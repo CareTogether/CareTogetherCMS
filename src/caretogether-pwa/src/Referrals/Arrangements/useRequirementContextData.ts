44db7a9fe3b546c83c35605c775282dc89166d65
PabloDinella <pablo.dinella@gmail.com>
2025-06-03 17:12:34 -0300

    Fix useRequirementContextData (some logic was left behind when splitting ArrangementCard into multiple files)

--- src/caretogether-pwa/src/Referrals/Arrangements/useRequirementContextData.ts
+++ src/caretogether-pwa/src/Referrals/Arrangements/useRequirementContextData.ts
@@ -14,54 +14,12 @@ export function useRequirementContextData(
 ) {
   const partneringFamilyId = partneringFamily.family!.id!;
 
-  const requirementContext: RequirementContext = {
+  const arrangementRequirementContext: RequirementContext = {
     kind: 'Arrangement',
     partneringFamilyId,
     referralId,
     arrangementId: arrangement.id!,
   };
-
-  const completedRequirementsWithContext = (
-    arrangement.completedRequirements || []
-  ).map((cr) => ({
-    completed: cr,
-    context: requirementContext,
-  }));
-
-  const exemptedRequirementsWithContext = (
-    arrangement.exemptedRequirements || []
-  ).map((er) => ({
-    exempted: er,
-    context: requirementContext,
-  }));
-
-  const missingRequirementsWithContext = (
-    arrangement.missingRequirements || []
-  ).map((requirement) => ({
-    missing: requirement,
-    context: requirementContext,
-  }));
-
-  // Sort the missing requirements so that all the items with due dates are shown after
-  // the items without due dates, and so that all items with due dates are shown in
-  // chronological order by due date.
-
-  const itemsWithoutDueDates = missingRequirementsWithContext.filter(
-    (item) => !item.missing.dueBy && !item.missing.pastDueSince
-  );
-  const itemsWithDueDates = missingRequirementsWithContext.filter(
-    (item) => item.missing.dueBy || item.missing.pastDueSince
-  );
-
-  itemsWithDueDates.sort((a, b) => {
-    const dateA =
-      a.missing.pastDueSince || a.missing.dueBy || '2000-01-01T00:00:00Z';
-    const dateB =
-      b.missing.pastDueSince || b.missing.dueBy || '2000-01-01T00:00:00Z';
-    return new Date(dateA).getTime() - new Date(dateB).getTime();
-  });
-
-  const mergedArray = [...itemsWithoutDueDates, ...itemsWithDueDates];
 
   const missingAssignmentFunctions =
     arrangementPolicy?.arrangementFunctions?.filter(
@@ -78,21 +36,161 @@ export function useRequirementContextData(
 
   const assignmentsMissingVariants =
     arrangementPolicy?.arrangementFunctions
-      ?.filter((functionPolicy) => functionPolicy.variants?.length)
+      ?.filter(
+        (functionPolicy) =>
+          functionPolicy.variants && functionPolicy.variants.length > 0
+      )
       .map(
         (functionPolicy) =>
           (arrangement.familyVolunteerAssignments?.filter(
             (fva) =>
               fva.arrangementFunction === functionPolicy.functionName &&
               !fva.arrangementFunctionVariant
-          ).length || 0) +
+          )?.length || 0) +
           (arrangement.individualVolunteerAssignments?.filter(
             (iva) =>
               iva.arrangementFunction === functionPolicy.functionName &&
               !iva.arrangementFunctionVariant
-          ).length || 0)
+          )?.length || 0)
       )
       .reduce((prev, curr) => prev + curr, 0) || 0;
+
+  const completedRequirementsWithContext = (
+    arrangement.completedRequirements || []
+  )
+    .map((cr) => ({
+      completed: cr,
+      context: arrangementRequirementContext as RequirementContext,
+    }))
+    .concat(
+      (arrangement.familyVolunteerAssignments || []).flatMap((fva) =>
+        (fva.completedRequirements || []).map((cr) => ({
+          completed: cr,
+          context: {
+            kind: 'Family Volunteer Assignment',
+            partneringFamilyId: partneringFamily.family!.id!,
+            referralId: referralId,
+            arrangementId: arrangement.id!,
+            assignment: fva,
+          } as RequirementContext,
+        }))
+      )
+    )
+    .concat(
+      (arrangement.individualVolunteerAssignments || []).flatMap((iva) =>
+        (iva.completedRequirements || []).map((cr) => ({
+          completed: cr,
+          context: {
+            kind: 'Individual Volunteer Assignment',
+            partneringFamilyId: partneringFamily.family!.id!,
+            referralId: referralId,
+            arrangementId: arrangement.id!,
+            assignment: iva,
+          },
+        }))
+      )
+    );
+
+  const exemptedRequirementsWithContext = (
+    arrangement.exemptedRequirements || []
+  )
+    .map((er) => ({
+      exempted: er,
+      context: arrangementRequirementContext as RequirementContext,
+    }))
+    .concat(
+      (arrangement.familyVolunteerAssignments || []).flatMap((fva) =>
+        (fva.exemptedRequirements || []).map((er) => ({
+          exempted: er,
+          context: {
+            kind: 'Family Volunteer Assignment',
+            partneringFamilyId: partneringFamily.family!.id!,
+            referralId: referralId,
+            arrangementId: arrangement.id!,
+            assignment: fva,
+          } as RequirementContext,
+        }))
+      )
+    )
+    .concat(
+      (arrangement.individualVolunteerAssignments || []).flatMap((iva) =>
+        (iva.exemptedRequirements || []).map((er) => ({
+          exempted: er,
+          context: {
+            kind: 'Individual Volunteer Assignment',
+            partneringFamilyId: partneringFamily.family!.id!,
+            referralId: referralId,
+            arrangementId: arrangement.id!,
+            assignment: iva,
+          },
+        }))
+      )
+    );
+
+  const missingRequirementsWithContext = (
+    arrangement.missingRequirements || []
+  ).map((requirement) => {
+    if (requirement.personId) {
+      return {
+        missing: requirement,
+        context: {
+          kind: 'Individual Volunteer Assignment',
+          partneringFamilyId: partneringFamily.family!.id!,
+          referralId: referralId,
+          arrangementId: arrangement.id!,
+          assignment: arrangement.individualVolunteerAssignments!.find(
+            (iva) =>
+              iva.arrangementFunction === requirement.arrangementFunction &&
+              iva.arrangementFunctionVariant ===
+                requirement.arrangementFunctionVariant &&
+              iva.familyId === requirement.volunteerFamilyId &&
+              iva.personId === requirement.personId
+          )!,
+        } as RequirementContext,
+      };
+    } else if (requirement.volunteerFamilyId) {
+      return {
+        missing: requirement,
+        context: {
+          kind: 'Family Volunteer Assignment',
+          partneringFamilyId: partneringFamily.family!.id!,
+          referralId: referralId,
+          arrangementId: arrangement.id!,
+          assignment: arrangement.familyVolunteerAssignments!.find(
+            (iva) =>
+              iva.arrangementFunction === requirement.arrangementFunction &&
+              iva.arrangementFunctionVariant ===
+                requirement.arrangementFunctionVariant &&
+              iva.familyId === requirement.volunteerFamilyId
+          )!,
+        } as RequirementContext,
+      };
+    } else {
+      return { missing: requirement, context: arrangementRequirementContext };
+    }
+  });
+
+  // Sort the missing requirements so that all the items with due dates are shown after
+  // the items without due dates, and so that all items with due dates are shown in
+  // chronological order by due date.
+
+  const itemsWithoutDueDates = missingRequirementsWithContext.filter(
+    (item) => !item.missing.dueBy && !item.missing.pastDueSince
+  );
+
+  const itemsWithDueDates = missingRequirementsWithContext.filter(
+    (item) => item.missing.dueBy || item.missing.pastDueSince
+  );
+
+  itemsWithDueDates.sort((a, b) => {
+    const dateA =
+      a.missing.pastDueSince || a.missing.dueBy || '2000-01-01T00:00:00Z';
+    const dateB =
+      b.missing.pastDueSince || b.missing.dueBy || '2000-01-01T00:00:00Z';
+    return new Date(dateA).getTime() - new Date(dateB).getTime();
+  });
+
+  const mergedArray = [...itemsWithoutDueDates, ...itemsWithDueDates];
 
   const upcomingRequirementsCount =
     arrangement.missingRequirements?.filter(
@@ -101,12 +199,12 @@ export function useRequirementContextData(
     ).length || 0;
 
   return {
+    missingAssignmentFunctions,
+    assignmentsMissingVariants,
     completedRequirementsWithContext,
     exemptedRequirementsWithContext,
     missingRequirementsWithContext,
     mergedArray,
-    missingAssignmentFunctions,
-    assignmentsMissingVariants,
     upcomingRequirementsCount,
   };
 }
