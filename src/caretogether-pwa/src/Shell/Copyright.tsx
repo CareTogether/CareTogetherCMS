6a537a58e2071419d68ced6fbe02ff4dae97ec9c
PabloDinella <pablo.dinella@gmail.com>
2025-09-01 16:50:22 -0300

    Refactor Copyright and Version components to remove unnecessary Box wrappers

--- src/caretogether-pwa/src/Shell/Copyright.tsx
+++ src/caretogether-pwa/src/Shell/Copyright.tsx
@@ -1,25 +1,23 @@
-import { Typography, Box } from '@mui/material';
+import { Typography } from '@mui/material';
 
 export function Copyright() {
   return (
-    <Box sx={{ textAlign: 'center' }}>
-      <Typography
-        variant="body2"
-        color="#fff8"
-        align="center"
-        sx={{ lineHeight: '2em' }}
+    <Typography
+      variant="body2"
+      color="#fff8"
+      align="center"
+      sx={{ lineHeight: '2em' }}
+    >
+      {' © '} {new Date().getFullYear()} &nbsp;
+      <a
+        style={{ color: '#fff8', textDecoration: 'none' }}
+        href="https://caretogether.io/"
+        target="_blank"
+        rel="noreferrer"
       >
-        {' © '} {new Date().getFullYear()} &nbsp;
-        <a
-          style={{ color: '#fff8', textDecoration: 'none' }}
-          href="https://caretogether.io/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          CareTogether CMS
-        </a>
-        <br />
-      </Typography>
-    </Box>
+        CareTogether CMS
+      </a>
+      <br />
+    </Typography>
   );
 }
--- src/caretogether-pwa/src/Shell/Version.tsx
+++ src/caretogether-pwa/src/Shell/Version.tsx
@@ -1,18 +1,16 @@
-import { Typography, Box } from '@mui/material';
+import { Typography } from '@mui/material';
 import { getAppVersion } from '../Utilities/appVersion';
 
 export function Version() {
   return (
-    <Box sx={{ textAlign: 'center' }}>
-      <Typography
-        variant="body2"
-        color="#fff8"
-        align="center"
-        sx={{ lineHeight: '1.5em' }}
-        className="ph-unmask"
-      >
-        Version: {getAppVersion()}
-      </Typography>
-    </Box>
+    <Typography
+      variant="body2"
+      color="#fff8"
+      align="center"
+      sx={{ lineHeight: '1.5em' }}
+      className="ph-unmask"
+    >
+      Version: {getAppVersion()}
+    </Typography>
   );
 }
