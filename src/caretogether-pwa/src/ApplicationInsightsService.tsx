9789535ffe0c9a23dbb29c84d4447cfaf64d22aa
PabloDinella <pablo.dinella@gmail.com>
2025-09-18 09:52:19 -0300

    Adjust application insights correlation headers configuration

--- src/caretogether-pwa/src/ApplicationInsightsService.tsx
+++ src/caretogether-pwa/src/ApplicationInsightsService.tsx
@@ -10,18 +10,32 @@ const appInsights = new ApplicationInsights({
   config: {
     connectionString: import.meta.env.VITE_APP_APPINSIGHTS_CONNECTIONSTRING,
     enableAutoRouteTracking: true,
-    enableCorsCorrelation: !import.meta.env.DEV, // Disable in development to avoid CORS issues
+    enableCorsCorrelation: true,
     enableRequestHeaderTracking: true,
     enableResponseHeaderTracking: true,
     enableAjaxErrorStatusText: true,
     enableAjaxPerfTracking: true,
+    // From
+    // https://learn.microsoft.com/en-us/azure/azure-monitor/app/javascript-sdk-configuration?tabs=javascriptwebsdkloaderscript#enable-w3c-distributed-tracing-support
+    //
+    // If the XMLHttpRequest or Fetch Ajax requests are sent to a different
+    // domain host, including subdomains, the correlation headers aren't
+    // included by default. To enable this feature, set the
+    // enableCorsCorrelation configuration field to true. If you set
+    // enableCorsCorrelation to true, all XMLHttpRequest and Fetch Ajax requests
+    // include the correlation headers. As a result, if the application on the
+    // server that is being called doesn't support the traceparent header, the
+    // request might fail, depending on whether the browser / version can
+    // validate the request based on which headers the server accepts. You can
+    // use the correlationHeaderExcludedDomains configuration field to exclude
+    // the server's domain from cross-component correlation header injection.
+    // For example, you can use correlationHeaderExcludedDomains:
+    // ['*.auth0.com'] to exclude correlation headers from requests sent to the
+    // Auth0 identity provider.
     correlationHeaderExcludedDomains: [
       '*.featurebase.app',
       'do.featurebase.app',
       'featurebase.app',
-      '*.googleapis.com',
-      '*.google.com',
-      '*.gstatic.com',
     ],
     extensions: [aiReactPlugin],
   },
