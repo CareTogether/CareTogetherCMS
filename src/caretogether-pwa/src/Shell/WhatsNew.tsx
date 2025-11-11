a0eb59eb9fe16f263a2c70d0591a74145df560d0
PabloDinella <pablo.dinella@gmail.com>
2025-11-11 11:42:58 -0300

    Simplify featurebase changelog integration

--- src/caretogether-pwa/src/Shell/WhatsNew.tsx
+++ src/caretogether-pwa/src/Shell/WhatsNew.tsx
@@ -2,14 +2,15 @@ import { Badge } from '@mui/material';
 import CampaignIcon from '@mui/icons-material/Campaign';
 import { useGlobalPermissions } from '../Model/SessionModel';
 import { Permission } from '../GeneratedClient';
-import { useFeaturebaseChangelog } from '../Hooks/useFeaturebaseChangelog';
 import { ListItemLink } from './ListItemLink';
+import { useRecoilValue } from 'recoil';
+import { changelogUnreadCountState } from '../Hooks/useFeaturebase';
 
 export default function WhatsNew() {
   const permissions = useGlobalPermissions();
   const hasAccess = permissions(Permission.AccessSupportScreen);
 
-  const unreadCount = useFeaturebaseChangelog();
+  const unreadCount = useRecoilValue(changelogUnreadCountState);
 
   if (!hasAccess) return null;
 
