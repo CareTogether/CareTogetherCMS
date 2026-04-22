20266570cc3596fd37a90e1edf893f462042577b
PabloDinella <pablo.dinella@gmail.com>
2026-04-22 18:54:34 -0300

    Refactor V1 referral notes model to use atomic command callbacks

--- src/caretogether-pwa/src/Model/V1ReferralNotesModel.ts
+++ src/caretogether-pwa/src/Model/V1ReferralNotesModel.ts
@@ -1,4 +1,3 @@
-import { useRecoilCallback, useSetRecoilState } from 'recoil';
 import {
   V1ReferralNoteRecordsCommand,
   V1ReferralNoteCommand,
@@ -8,128 +7,89 @@ import {
   ApproveV1ReferralNote,
   UpdateV1ReferralNoteAccessLevel,
 } from '../GeneratedClient';
-import { api } from '../Api/Api';
-import { selectedLocationContextState, visibleAggregatesState } from './Data';
 import { commandFactory } from './CommandFactory';
+import { useAtomicRecordsCommandCallback } from './Data';
+
+function useV1ReferralNoteCommandCallback<T extends unknown[]>(
+  callback: (
+    referralId: string,
+    ...args: T
+  ) => Promise<V1ReferralNoteCommand>
+) {
+  return useAtomicRecordsCommandCallback(async (referralId, ...args: T) => {
+    const wrapper = new V1ReferralNoteRecordsCommand();
+    wrapper.command = await callback(referralId, ...args);
+    return wrapper;
+  });
+}
 
 export function useV1ReferralNotesModel() {
-  const setVisibleAggregates = useSetRecoilState(visibleAggregatesState);
-
-  const submitReferralNoteCommand = useRecoilCallback(
-    ({ snapshot }) =>
-      async (command: V1ReferralNoteCommand): Promise<void> => {
-        const { organizationId, locationId } = await snapshot.getPromise(
-          selectedLocationContextState
-        );
-
-        const wrapper = new V1ReferralNoteRecordsCommand();
-        wrapper.command = command;
-
-        await api.records.submitAtomicRecordsCommand(
-          organizationId,
-          locationId,
-          wrapper
-        );
-
-        const allVisibleAggregates = await api.records.listVisibleAggregates(
-          organizationId,
-          locationId
-        );
-
-        setVisibleAggregates(allVisibleAggregates);
-      },
-    [setVisibleAggregates]
-  );
-
-  const createDraftReferralNote = useRecoilCallback(
-    () =>
-      async (
-        referralId: string,
-        noteId: string,
-        draftNoteContents: string,
-        backdatedTimestampUtc?: Date,
-        accessLevel?: string
-      ) => {
-        const command = commandFactory(CreateV1ReferralDraftNote, {
-          referralId,
-          noteId,
-          draftNoteContents,
-          backdatedTimestampUtc: backdatedTimestampUtc ?? undefined,
-          accessLevel: accessLevel ?? undefined,
-        });
-
-        await submitReferralNoteCommand(command);
-      },
-    [submitReferralNoteCommand]
-  );
-
-  const editDraftReferralNote = useRecoilCallback(
-    () =>
-      async (
-        referralId: string,
-        noteId: string,
-        draftNoteContents: string,
-        backdatedTimestampUtc?: Date,
-        accessLevel?: string
-      ) => {
-        const command = commandFactory(EditV1ReferralDraftNote, {
-          referralId,
-          noteId,
-          draftNoteContents,
-          backdatedTimestampUtc: backdatedTimestampUtc ?? undefined,
-          accessLevel: accessLevel ?? undefined,
-        });
-
-        await submitReferralNoteCommand(command);
-      },
-    [submitReferralNoteCommand]
-  );
-
-  const discardDraftReferralNote = useRecoilCallback(
-    () => async (referralId: string, noteId: string) => {
-      const command = commandFactory(DiscardV1ReferralDraftNote, {
+  const createDraftReferralNote = useV1ReferralNoteCommandCallback(
+    async (
+      referralId: string,
+      noteId: string,
+      draftNoteContents: string,
+      backdatedTimestampUtc?: Date,
+      accessLevel?: string
+    ) =>
+      commandFactory(CreateV1ReferralDraftNote, {
         referralId,
         noteId,
-      });
-
-      await submitReferralNoteCommand(command);
-    },
-    [submitReferralNoteCommand]
+        draftNoteContents,
+        backdatedTimestampUtc: backdatedTimestampUtc ?? undefined,
+        accessLevel: accessLevel ?? undefined,
+      })
   );
 
-  const approveReferralNote = useRecoilCallback(
-    () =>
-      async (
-        referralId: string,
-        noteId: string,
-        finalizedNoteContents: string,
-        backdatedTimestampUtc?: Date,
-        accessLevel?: string
-      ) => {
-        const command = commandFactory(ApproveV1ReferralNote, {
-          referralId,
-          noteId,
-          finalizedNoteContents,
-          backdatedTimestampUtc: backdatedTimestampUtc ?? undefined,
-          accessLevel: accessLevel ?? undefined,
-        });
-
-        await submitReferralNoteCommand(command);
-      },
-    [submitReferralNoteCommand]
+  const editDraftReferralNote = useV1ReferralNoteCommandCallback(
+    async (
+      referralId: string,
+      noteId: string,
+      draftNoteContents: string,
+      backdatedTimestampUtc?: Date,
+      accessLevel?: string
+    ) =>
+      commandFactory(EditV1ReferralDraftNote, {
+        referralId,
+        noteId,
+        draftNoteContents,
+        backdatedTimestampUtc: backdatedTimestampUtc ?? undefined,
+        accessLevel: accessLevel ?? undefined,
+      })
   );
 
-  const updateReferralNoteAccessLevel = useRecoilCallback(
-    () => async (referralId: string, noteId: string, accessLevel?: string) => {
-      const command = commandFactory(UpdateV1ReferralNoteAccessLevel, {
+  const discardDraftReferralNote = useV1ReferralNoteCommandCallback(
+    async (referralId: string, noteId: string) =>
+      commandFactory(DiscardV1ReferralDraftNote, {
+        referralId,
+        noteId,
+      })
+  );
+
+  const approveReferralNote = useV1ReferralNoteCommandCallback(
+    async (
+      referralId: string,
+      noteId: string,
+      finalizedNoteContents: string,
+      backdatedTimestampUtc?: Date,
+      accessLevel?: string
+    ) =>
+      commandFactory(ApproveV1ReferralNote, {
+        referralId,
+        noteId,
+        finalizedNoteContents,
+        backdatedTimestampUtc: backdatedTimestampUtc ?? undefined,
+        accessLevel: accessLevel ?? undefined,
+      })
+  );
+
+  const updateReferralNoteAccessLevel = useV1ReferralNoteCommandCallback(
+    async (referralId: string, noteId: string, accessLevel?: string) =>
+      commandFactory(UpdateV1ReferralNoteAccessLevel, {
         referralId,
         noteId,
         accessLevel: accessLevel ?? undefined,
-      });
-
-      await submitReferralNoteCommand(command);
-    },
-    [submitReferralNoteCommand]
+      })
   );
 
   return {
